bc3cc6476faada438e6e4be40206a2409f181e13
marapredescu1 <mara.predescu@eaudeweb.ro>
2017-07-25 15:46:31 +0300

    Navbar work

--- docroot/themes/iucn_who/js/navbar-headroom.js
+++ docroot/themes/iucn_who/js/navbar-headroom.js
@@ -1,7 +1,7 @@
 (function (Headroom) {
   // grab an element
 
-  var myElement = document.querySelector("header.navbar-fixed-top");
+  var myElement = document.querySelector("header.navbar");
   // construct an instance of Headroom, passing the element
   if(myElement) {
     var headroom  = new Headroom(myElement);
