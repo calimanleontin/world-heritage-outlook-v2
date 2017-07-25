7405f608425d020047b24d3565f71f2fa3906638
marapredescu1 <mara.predescu@eaudeweb.ro>
2017-07-25 12:13:12 +0300

    Outlines are hidden unless navigating with keyboard

--- docroot/themes/iucn_who/js/main.js
+++ docroot/themes/iucn_who/js/main.js
@@ -11,6 +11,19 @@
 
     $('#navbar-collapse').on('show.bs.collapse', blockBodyScroll);
     $('#navbar-collapse').on('hide.bs.collapse', unblockBodyScroll);
+
+    // Shows outlines if navigating with keyboard
+    document.addEventListener('keydown', function(e) {
+        if (e.keyCode === 9) {
+        $body.addClass('show-focus-outlines');
+        }
+    });
+    document.addEventListener('touchstart', function() {
+        $body.removeClass('show-focus-outlines');
+    });
+    document.addEventListener('mousedown', function() {
+        $body.removeClass('show-focus-outlines');
+    });
   });
 
 }(jQuery));
