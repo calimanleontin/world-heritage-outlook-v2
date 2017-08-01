485db3fc9818168658e20fd0f85c6b3cfa318274
Cristian Romanescu <cristian@romanescu.ro>
2017-08-01 20:03:32 +0300

    Implemented colouring and filtering of sites

--- docroot/modules/iucn/iucn_who_homepage/js/map_homepage.js
+++ docroot/modules/iucn/iucn_who_homepage/js/map_homepage.js
@@ -6,18 +6,80 @@
 function postInitMap(instance_id, map, config) {
   (function ($, Drupal, drupalSettings) {
     'use strict';
+    var icon1 = {
+      path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
+      fillColor: 'gold',
+      fillOpacity: 1,
+      scale: 0.01,
+      strokeColor: 'gold',
+      strokeWeight: 10
+    };
+    var icon2 = {
+      path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
+      fillColor: 'green',
+      fillOpacity: 1,
+      scale: 0.01,
+      strokeColor: 'green',
+      strokeWeight: 10
+    };
+    var icon3 = {
+      path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
+      fillColor: 'red',
+      fillOpacity: 1,
+      scale: 0.01,
+      strokeColor: 'red',
+      strokeWeight: 10
+    };
+    var icon4 = {
+      path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
+      fillColor: 'blue',
+      fillOpacity: 1,
+      scale: 0.01,
+      strokeColor: 'blue',
+      strokeWeight: 10
+    };
+    var icon5 = {
+      path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
+      fillColor: 'black',
+      fillOpacity: 1,
+      scale: 0.01,
+      strokeColor: 'black',
+      strokeWeight: 10
+    };
+    var icon6 = {
+      path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
+      fillColor: 'orange',
+      fillOpacity: 1,
+      scale: 0.01,
+      strokeColor: 'orange',
+      strokeWeight: 10
+    };
+    var $markers = [];
     for(var $i = 0; $i < config.markers.length; $i++) {
       var $mc = config.markers[$i];
       var $marker = new google.maps.Marker({
         map: map,
-        animation: google.maps.Animation.DROP,
         position: new google.maps.LatLng($mc.lat, $mc.lng),
+        icon: eval('icon' + $mc.status_id),
         customInfo: $mc
       });
       $marker.addListener('click', function() {
         $('#map-site-details').html(this.customInfo.render);
       });
+      $markers.push($marker);
     }
+
+
+    $('#map-filters a').on('click', function() {
+      var $filter_status_id = $(this).data('filter');
+      for(var $i = 0; $i < $markers.length; $i++) {
+        var $marker = $markers[$i];
+        var $visible =  $filter_status_id == 'all'
+            || parseInt($filter_status_id) == parseInt($marker.customInfo.status_id);
+        $marker.setVisible($visible);
+      }
+      return false;
+    });
   })(jQuery, Drupal, drupalSettings);
 }
 
--- docroot/modules/iucn/iucn_who_homepage/src/Plugin/Block/HomePageGoogleMapsBlock.php
+++ docroot/modules/iucn/iucn_who_homepage/src/Plugin/Block/HomePageGoogleMapsBlock.php
@@ -3,7 +3,10 @@
 namespace Drupal\iucn_who_homepage\Plugin\Block;
 
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+use Drupal\image\Entity\ImageStyle;
 use Drupal\iucn_who_core\Plugin\Block\GoogleMapsBaseBlock;
+use Drupal\iucn_who_core\Sites\SitesQueryUtil;
 
 
 /**
@@ -33,26 +36,85 @@ class HomePageGoogleMapsBlock extends GoogleMapsBaseBlock {
     $content['output'] = [
       '#theme' => 'homepage_map_block',
       '#markup_map' => parent::getMapMarkup(),
+      '#sites_total_count' => SitesQueryUtil::getPublishedSitesCount(),
     ];
     return $content;
   }
 
   private function getMarkers() {
-    $detail = [
-      '#theme' => 'homepage_map_site_detail',
-      '#status' => 'Critical',
-    ];
-
     $ret = [];
-    $ret[] = [
-      'lat' => '23', 'lng' => '133',
-      'title' => 'Simple Marker',
-      'status' => 'Critical',
-      'thumbnail' => 'http://who.local/sites/default/files/site/Tassili_n%27Ajjer_National_Park.jpg',
-      'country' => 'Algeria',
-      'inscription_year' => 1986,
-      'render' => \Drupal::service('renderer')->render($detail),
-    ];
+    $sites = SitesQueryUtil::getPublishedSites();
+    foreach($sites as $node) {
+      $latitude = $node->field_coordinate_y->value;
+      $longitude = $node->field_coordinate_x->value;
+      if (empty($latitude) || empty($longitude)) {
+        continue;
+      }
+      $status_id = $this->getSiteStatus($node);
+      $detail = [
+        '#theme' => 'homepage_map_site_detail',
+        '#title' => $node->title->value,
+        '#status' => [
+          'label' => 'TODO',
+          'id' => $status_id,
+        ],
+        '#country' => [
+          'label' => $this->getSiteCountryLabel($node)
+        ],
+        '#thumbnail' => $this->getSiteThumbnail($node),
+        '#inscription' => $this->getSiteInscriptionYear($node),
+        '#link' => Url::fromRoute('entity.node.canonical', array('node' => $node->id())),
+        '#stat_values' => 'TODO',
+        '#stat_threat' => 'TODO',
+        '#stat_protection' => 'TODO',
+      ];
+      $ret[] = [
+        'lat' => $latitude,
+        'lng' => $longitude,
+        'title' => $node->title->value,
+        'status_label' => 'TODO',
+        'status_id' => $status_id,
+        'thumbnail' => $this->getSiteThumbnail($node),
+        'country' => 'Algeria',
+        'inscription_year' => $this->getSiteInscriptionYear($node),
+        'render' => \Drupal::service('renderer')->render($detail),
+      ];
+    }
     return $ret;
+  }
+
+
+  private function getSiteCountryLabel($node) {
+    $countries = [];
+    try {
+      if (count($node->field_country) > 0) {
+        foreach ($node->field_country as $ob) {
+          $countries[] = $ob->entity->name->value;
+        }
+      }
+    } catch (\Exception $e) {
+      // @todo log
+    }
+    return implode(', ', $countries);
+  }
+
+  private function getSiteThumbnail($node, $style = 'sites_thumbnail') {
+    $style = ImageStyle::load($style);
+    if (!empty($node->field_image->entity)) {
+      return $style->buildUrl($node->field_image->entity->getFileUri());
+    }
+  }
+
+  private function getSiteInscriptionYear($node) {
+    if (!empty($node->field_inscription_year->value)) {
+      return date('Y', strtotime($node->field_inscription_year->value));
+    }
+  }
+
+  private function getSiteStatus($node) {
+    // @TODO
+    $array = ['1' => '1', '2' => '2', '3' => '3', '4' => '4', '5' => '5', '6' => '6'];
+    $k = array_rand($array);
+    return $array[$k];
   }
 }
