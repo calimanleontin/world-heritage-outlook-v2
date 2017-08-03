798b47d639292d3fd9a0c30391423238e618ac90
Cristian Romanescu <cristian@romanescu.ro>
2017-08-03 11:05:13 +0300

    Improve the sync script

--- install_local.sh
+++ install_local.sh
@@ -1,31 +1,53 @@
 #!/bin/bash
+
+#!/bin/bash
 # Go to docroot/
-cd docroot/
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+WHITE='\033[1;37m'
+
+# Get the full path to the directory containing this script.
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd "$SCRIPT_DIR/docroot"
 
 env="iucn.test"
 if [ ! -z "$1" ]; then
   env=$1
 fi
 
-echo "Dropping all tables in database..."
+echo -e "${GREEN}Dropping all tables in database...${WHITE}"
 drush sql-drop -y
-echo
+if [ $? -ne 0 ]; then
+  echo -e "${RED}Failed to drop the database, aborting ...${WHITE}\n";
+  exit -1
+fi
 
-#echo "Getting '$env' environment database..."
+echo -e "${GREEN}Getting '$env' environment database...${WHITE}"
 drush sql-sync "@$env" @self -y
+if [ $? -ne 0 ]; then
+  echo -e "${RED}Failed to import the $env database, aborting ...${WHITE}\n";
+  exit -1
+fi
 
-#echo "Importing configuration..."
+echo -e "${GREEN}Importing default configuration...${WHITE}"
 drush csim -y
+if [ $? -ne 0 ]; then
+  echo -e "${RED}Failed to import the default configuration, aborting ...${WHITE}\n";
+  exit -1
+fi
 
-echo "Running database pending updates..."
+
+echo -e "${GREEN}Running database pending updates...${WHITE}"
 drush updatedb -y
-echo
 
-#echo "Rsync files..."
+echo -e "${GREEN}Updating entities...${WHITE}"
+drush entup -y
+
+echo -e "${GREEN}Syncing remote files ...${WHITE}"
 drush -y rsync "@$env":%files @self:%files
 
-#echo "Updating iucn user..."
+echo -e "${GREEN}Setting iucn password ...${WHITE}"
 drush upwd iucn --password="password"
 
 drush cr
-echo "Done!"
