3df354d9872388121daeea00d2c969b666b47ac1
marapredescu1 <mara.predescu@eaudeweb.ro>
2017-08-28 15:57:34 +0300

    Search page top block styling

--- docroot/themes/iucn_who/js/main.js
+++ docroot/themes/iucn_who/js/main.js
@@ -34,6 +34,16 @@
         ];
         var vhFix = new VHChromeFix(vhFixOptions);
     }
+    $.ui.autocomplete.prototype._resizeMenu = function () {
+      var ul = this.menu.element;
+      var $inputGroup = this.element.closest('.input-group');
+      if($inputGroup.is(this.element.parent())) {
+        ul.outerWidth($inputGroup.outerWidth());
+      }
+      else {
+        ul.outerWidth(this.element.outerWidth());
+      }
+    }
   });
 
 }(jQuery));
