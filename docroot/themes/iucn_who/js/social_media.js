433930fb19ed5c31e4806f2e8314651c92458acd
stefanbutura <stefan_butura@yahoo.com>
2017-09-01 17:35:00 +0300

    social media js + fixes

--- docroot/themes/iucn_who/js/social_media.js
+++ docroot/themes/iucn_who/js/social_media.js
@@ -1,3 +1,28 @@
-/**
- * Created by stefan on 9/1/17.
- */
+(function($) {
+    $('.twitter, .facebook-share, .linkedin').click(function(e) {
+        e.preventDefault();
+        var width  = 575,
+            height = 400,
+            left   = ($(window).width()  - width)  / 2,
+            top    = ($(window).height() - height) / 2,
+            url    = this.href,
+            opts   = 'status=1' +
+                ',width='  + width  +
+                ',height=' + height +
+                ',top='    + top    +
+                ',left='   + left;
+        var title;
+        switch(this.className) {
+            case 'facebook-share share':
+                title = 'facebook_share';
+                break;
+            case 'twitter share':
+                title = 'twitter_share';
+                break;
+            case 'linkedin share':
+                title = 'linkedin_share';
+                break;
+        }
+        window.open(url, title, opts);
+    });
+})(jQuery);
