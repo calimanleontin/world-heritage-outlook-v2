863585490878067dbcb41a7142cf35514d4e8d06
marapredescu1 <mara.predescu@eaudeweb.ro>
2017-09-13 17:25:59 +0300

    Assessments mobile WIP

--- docroot/themes/iucn_who/js/main.js
+++ docroot/themes/iucn_who/js/main.js
@@ -46,6 +46,25 @@
           }
         }
     }
+
+    var $document  = $(document);
+    var $body = $('body');
+    var $sidemenu = $('#sidemenu');
+    var $sidemenuToggle = $('#sidemenu-toggle');
+    var $modalBackdrop = $('.modal-backdrop');
+
+    $sidemenu.on('show.bs.dropdown', function () {
+      $body.addClass('sidemenu-open');
+      $modalBackdrop.show().addClass('in');
+    })
+    $sidemenu.on('hide.bs.dropdown', function () {
+      $body.removeClass('sidemenu-open');
+      $modalBackdrop.hide().removeClass('in');
+    })
+
+    $document.on('click', '#sidemenu', function (e) {
+      e.stopPropagation();
+    });
   });
 
 }(jQuery));
