32b32c6d728764997985d1420245bbd34ddac50a
marapredescu1 <mara.predescu@eaudeweb.ro>
2017-10-05 17:48:20 +0300

    Navbar is fixed on desktop too and hides when scrolling past it

--- docroot/themes/iucn_who/js/navbar-headroom.js
+++ docroot/themes/iucn_who/js/navbar-headroom.js
@@ -1,10 +1,13 @@
 (function (Headroom) {
+  'use strict';
   // grab an element
 
   var myElement = document.querySelector("header.navbar");
   // construct an instance of Headroom, passing the element
   if(myElement) {
-    var headroom  = new Headroom(myElement);
+    var headroom  = new Headroom(myElement, {
+      offset : myElement.offsetHeight
+    });
     // initialise
     headroom.init();
   }
