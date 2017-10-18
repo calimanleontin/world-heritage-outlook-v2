4f0427acbdb7a57747dc010bee595a35ed31a242
vadimski <vadeemk@gmail.com>
2017-10-18 12:12:14 +0300

    Migrate sites updates

--- docroot/modules/iucn/iucn_migrate/src/Plugin/migrate/process/Coordinate.php
+++ docroot/modules/iucn/iucn_migrate/src/Plugin/migrate/process/Coordinate.php
@@ -21,18 +21,9 @@ class Coordinate extends ProcessPluginBase {
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
     $url = "http://services5.arcgis.com/Mj0hjvkNtV7NRhA7/arcgis/rest/services/Latest_WH/FeatureServer/0/query?where=wdpaid%3D" . $value . "&objectIds=&time=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=true&outFields=&returnGeometry=true&returnCentroid=true&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnDistinctValues=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&quantizationParameters=&sqlFormat=none&f=pjson&token=";
 
-    if(isset($this->configuration['type']) && $this->configuration['type'] == 'x'){
-      $data = json_decode(file_get_contents($url));
-      if($data && isset($data->features[0]->centroid->x)){
-        return $data->features[0]->centroid->x;
-      }
-    }
-
-    if(isset($this->configuration['type']) && $this->configuration['type'] == 'y'){
-      $data = json_decode(file_get_contents($url));
-      if($data && isset($data->features[0]->centroid->y)){
-        return $data->features[0]->centroid->y;
-      }
+    $data = json_decode(file_get_contents($url));
+    if ($data && isset($data->features[0]->centroid->x) && isset($data->features[0]->centroid->y)) {
+      return 'POINT (' . $data->features[0]->centroid->x . ' ' . $data->features[0]->centroid->y . ')';
     }
     return FALSE;
   }
