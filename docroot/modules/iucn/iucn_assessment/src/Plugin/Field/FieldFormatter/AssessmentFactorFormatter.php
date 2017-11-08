7ae0ce0dc476e43c7e5702812c35040e10dc68d7
vadimski <vadeemk@gmail.com>
2017-11-08 17:30:33 +0200

    Updated Community benefits

--- docroot/modules/iucn/iucn_assessment/src/Plugin/Field/FieldFormatter/AssessmentFactorFormatter.php
+++ docroot/modules/iucn/iucn_assessment/src/Plugin/Field/FieldFormatter/AssessmentFactorFormatter.php
@@ -25,24 +25,39 @@ class AssessmentFactorFormatter extends FormatterBase {
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $element = [];
+
+    $types = [
+      'assessment_benefits_impact_level' => $this->t('Impact level'),
+      'assessment_benefits_impact_trend' => $this->t('Trend'),
+    ];
+
+    $fields = [
+      'field_as_benefits_commun_in' => $this->t('Community within site'),
+      'field_as_benefits_commun_out' => $this->t('Community outside site'),
+      'field_as_benefits_commun_wide' => $this->t('Wider Community (global)'),
+    ];
+
     foreach ($items as $delta => $item) {
       if (empty($item->entity) || empty($item->entity->name)) {
         continue;
       }
-
-      $types = [
-        'assessment_benefits_impact_level' => 'Impact level',
-        'assessment_benefits_impact_trend' => 'Trend',
-      ];
-
-      $value = $item->entity->name->value;
-      $term_type = $this->t($types[$item->entity->vid->target_id]);
-      $markup = $term_type . ' - ' . $value;
+      $term_type = NULL;
+      if (isset($types[$item->entity->vid->target_id])) {
+        $term_type = $types[$item->entity->vid->target_id];
+      }
+      else {
+        /** @var FieldItemListInterface $item*/
+        $field_name = $item->getFieldDefinition()->getName();
+        if (isset($fields[$field_name])) {
+          $term_type = $fields[$field_name];
+        }
+      }
       $element[$delta] = [
-        '#type' => 'markup',
-        '#markup' => $markup,
+          '#type' => 'markup',
+          '#markup' => ($term_type ? $term_type . ' - ' : '' ) . $item->entity->name->value,
       ];
     }
+
     return $element;
   }
 
