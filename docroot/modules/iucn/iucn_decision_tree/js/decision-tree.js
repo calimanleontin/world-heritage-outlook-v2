3d2aa62737a2b1b2de72fc39aecf0cb68e0b5019
vadimski <vadeemk@gmail.com>
2017-12-21 17:08:07 +0200

    Updated javascript

--- docroot/modules/iucn/iucn_decision_tree/js/decision-tree.js
+++ docroot/modules/iucn/iucn_decision_tree/js/decision-tree.js
@@ -4,13 +4,17 @@
       $( document ).on( "click", "a[rel=load-decisions]", function() {
         var $decisions = $(this).data('decisions');
         var $container = $(this).data('container');
+        var $container_element = $('#' + $(this).data('container'));
+        $container_element.empty();
         $('a[data-container=' + $container + ']').removeClass('active');
-        $(this).addClass('active');
+        $link_element = $(this);
+        $container_element.addClass('media--loading');
         $.ajax({
           url: '/decision_tree/nodes/' + $decisions,
           type: 'GET',
         }).done(function($data) {
-          $('#' + $container).html($data);
+          $container_element.hide().html($data).removeClass('media--loading').fadeIn();
+          $link_element.addClass('active');
         });
         return false;
       });
