a06f2b9c9bfd38732947147870eeb7706bf572e3
vadimski <vadeemk@gmail.com>
2018-01-11 13:11:16 +0200

    282-review-of-pdf-fixes

--- docroot/modules/iucn/iucn_site/src/Plugin/DsField/Header.php
+++ docroot/modules/iucn/iucn_site/src/Plugin/DsField/Header.php
@@ -14,24 +14,27 @@ use Drupal\Core\Link;
  *   entity_type = "node"
  * )
  */
-
 class Header extends DsFieldBase {
+
   /**
    * {@inheritdoc}
    */
-
   public function build() {
     global $base_url;
     /* @var $node \Drupal\node\NodeInterface */
     $node = $this->entity();
-
+    $assessment_year = iucn_pdf_assessment_year_display($node);
     $element = [
       '#type' => 'html_tag',
       '#tag' => 'header',
       '#attributes' => ['class' => []],
       '#value' => '<em>' . t('IUCN World Heritage Outlook: @link', [
         '@link' => Link::createFromRoute($base_url, '<front>')->toString(),
-      ]) . '<br/>' . $node->getTitle() . ' - ' . iucn_pdf_assessment_year_display($node) . ' ' .t('Conservation Outlook Assessment') . '</em>',
+      ]) . '<br/>' .
+      $node->getTitle() . ' - ' . iucn_pdf_assessment_year_display($node) . ' ' .
+      t('Conservation Outlook Assessment  @archived', [
+        '@archived' => $assessment_year != \Drupal::service('iucn_assessment.assessments_year')->current() ? '(' . t('archived') . ')' : '',
+      ]) . '</em>',
       '#suffix' => '<div class="pdf-header-cover"></div>',
     ];
     return $element;
@@ -46,4 +49,5 @@ class Header extends DsFieldBase {
     }
     return parent::isAllowed();
   }
+
 }
--- docroot/modules/iucn/iucn_site/src/Plugin/DsField/LatestAssessmentNotice.php
+++ docroot/modules/iucn/iucn_site/src/Plugin/DsField/LatestAssessmentNotice.php
@@ -3,6 +3,8 @@
 namespace Drupal\iucn_site\Plugin\DsField;
 
 use Drupal\ds\Plugin\DsField\DsFieldBase;
+use Drupal\Core\Url;
+use Drupal\Core\Link;
 
 /**
  * Plugin that renders the author of a node.
@@ -13,8 +15,8 @@ use Drupal\ds\Plugin\DsField\DsFieldBase;
  *   entity_type = "node"
  * )
  */
-
 class LatestAssessmentNotice extends DsFieldBase {
+
   /**
    * {@inheritdoc}
    */
@@ -22,44 +24,45 @@ class LatestAssessmentNotice extends DsFieldBase {
     global $base_url, $_iucn_assessment_is_latest_assessment;
     $elements = [];
     if (empty($_iucn_assessment_is_latest_assessment)) {
-    /* @var $node \Drupal\node\NodeInterface */
-    $node = $this->entity();
+      /* @var $node \Drupal\node\NodeInterface */
+      $node = $this->entity();
+      $latest_url = Url::fromRoute('iucn_pdf.download', ['entity_id' => $node->id()]);
+      $latest_url->setAbsolute(TRUE);
 
-    $latest_url = \Drupal\Core\Url::fromRoute('iucn_pdf.download', ['entity_id' => $node->id()]);
-    $latest_url->setAbsolute(TRUE);
+      $assessment_year = iucn_pdf_assessment_year_display($node);
 
-    $elements[] = [
-      '#type' => 'html_tag',
-      '#tag' => 'div',
-      '#attributes' => ['class' => []],
-      '#value' => t('IUCN Conservation Outlook Assessment @year', [
-        '@year' => iucn_pdf_assessment_year_display($node),
-      ]),
-    ];
-
-    if ($finalised_date = iucn_pdf_assessment_finalised_display($node)) {
-      $date = \DateTime::createFromFormat('Y-m-d', $finalised_date);
-      $finalised = $date->format('d F Y');
       $elements[] = [
         '#type' => 'html_tag',
         '#tag' => 'div',
         '#attributes' => ['class' => []],
-        '#value' => t('Finalised on @finalised', [
-          '@finalised' => $finalised,
+        '#value' => t('IUCN Conservation Outlook Assessment @year <strong>(@archived)</strong>', [
+          '@year' => $assessment_year,
+          '@archived' => t('archived'),
         ]),
       ];
-    }
 
+      if ($finalised_date = iucn_pdf_assessment_finalised_display($node)) {
+        $date = \DateTime::createFromFormat('Y-m-d', $finalised_date);
+        $finalised = $date->format('d F Y');
+        $elements[] = [
+          '#type' => 'html_tag',
+          '#tag' => 'div',
+          '#attributes' => ['class' => []],
+          '#value' => t('Finalised on @finalised', [
+            '@finalised' => $finalised,
+          ]),
+        ];
+      }
 
-    $elements[] = [
-      '#type' => 'html_tag',
-      '#tag' => 'div',
-      '#attributes' => ['class' => ['latest-assessment-notice']],
-      '#value' => t('Please note: this is an archived Conservation Outlook Assessment for @site. To access the most up-to-date Conservation Outlook Assessment for this site, please visit @link.', [
-        '@site' => $node->getTitle(),
-        '@link' => \Drupal\Core\Link::fromTextAndUrl($base_url, $latest_url)->toString(),
-      ]),
-    ];
+      $elements[] = [
+        '#type' => 'html_tag',
+        '#tag' => 'div',
+        '#attributes' => ['class' => ['latest-assessment-notice']],
+        '#value' => t('Please note: this is an archived Conservation Outlook Assessment for @site. To access the most up-to-date Conservation Outlook Assessment for this site, please visit @link.', [
+          '@site' => $node->getTitle(),
+          '@link' => Link::fromTextAndUrl($base_url, $latest_url)->toString(),
+        ]),
+      ];
     }
     return $elements;
   }
@@ -73,4 +76,5 @@ class LatestAssessmentNotice extends DsFieldBase {
     }
     return parent::isAllowed();
   }
+
 }
