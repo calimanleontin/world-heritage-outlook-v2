7baaa9dc1ff094a2b51da589c6065f4f999cb142
marapredescu1 <mara.predescu@eaudeweb.ro>
2018-01-16 12:20:28 +0200

    Adjusted styling for decision tree

--- docroot/modules/iucn/iucn_decision_tree/js/decision-tree.js
+++ docroot/modules/iucn/iucn_decision_tree/js/decision-tree.js
@@ -2,20 +2,26 @@
   Drupal.behaviors.load_decision = {
     attach: function (context, settings) {
       $( document ).on( "click", "a[rel=load-decisions]", function() {
-        var $decisions = $(this).data('decisions');
-        var $container = $(this).data('container');
-        var $container_element = $('#' + $(this).data('container'));
+        var $this = $(this);
+        var $decisions = $this.data('decisions');
+        var $container = $this.data('container');
+        var $container_element = $('#' + $this.data('container'));
+
 
         $container_element.empty();
         $('a[data-container=' + $container + ']').removeClass('active');
-        $link_element = $(this);
         $container_element.addClass('media--loading');
         $.ajax({
           url: '/decision_tree/nodes/' + $decisions,
           type: 'GET',
         }).done(function($data) {
           $container_element.hide().html($data).removeClass('media--loading').fadeIn();
-          $link_element.addClass('active');
+          $this.addClass('active');
+          $this.parent().addClass('active-inside');
+          $this.closest('.iucn-who-decision-tree-group')
+            .siblings('.iucn-who-decision-tree-group').removeClass('active-group')
+            .end()
+            .addClass('active-group');
         });
         return false;
       });
