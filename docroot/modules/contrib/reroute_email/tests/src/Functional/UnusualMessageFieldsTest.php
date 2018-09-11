d03d0c93e0a24def201a31e16b4b2df08f560569
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2018-09-11 12:10:52 +0300

    reroute_email update

--- docroot/modules/contrib/reroute_email/tests/src/Functional/UnusualMessageFieldsTest.php
+++ docroot/modules/contrib/reroute_email/tests/src/Functional/UnusualMessageFieldsTest.php
@@ -1,12 +1,16 @@
 <?php
-namespace Drupal\reroute_email\Tests;
 
-use Drupal\reroute_email\RerouteEmailTestBase;
+namespace Drupal\Tests\reroute_email\Functional;
+
+use Drupal\Component\Render\FormattableMarkup;
 
 /**
- * Test handling of
+ * Test handling of unusual fields.
+ *
  * - message body passed as a string
  * - Cc/Bcc header keys with an unexpected case.
+ *
+ * @ingroup reroute_email_tests
  *
  * @group reroute_email
  */
@@ -37,36 +41,37 @@ class UnusualMessageFieldsTest extends RerouteEmailTestBase {
     $test_cc_key = 'cC';
     $test_bcc_key = 'bCc';
 
-    // Configure to reroute normally to rerouted@example.com.
-    $this->configureRerouteEmail();
+    // Configure to reroute to {$this->rerouteDestination}.
+    $this->configureRerouteEmail(TRUE, $this->rerouteDestination);
 
     // Print test email values for comparing values on test results page.
-    $test_message = array(
+    $test_message = [
       'to' => $this->originalDestination,
-      'params' => array(
-        'body' => "Test Message body is a string.",
-        'headers' => array(
+      'params' => [
+        'body' => 'Test Message body is a string.',
+        'headers' => [
           'test_cc_key' => $test_cc_key,
           'test_bcc_key' => $test_bcc_key,
-          $test_cc_key => "test_cc_key@example.com",
-          $test_bcc_key => "test_bcc_key@example.com",
-        ),
-      ),
-    );
+          $test_cc_key => 'test_cc_key@example.com',
+          $test_bcc_key => 'test_bcc_key@example.com',
+        ],
+      ],
+    ];
     // Send test helper sub-module's email.
     $langcode = \Drupal::languageManager()->getCurrentLanguage()->getId();
     \Drupal::getContainer()
       ->get('plugin.manager.mail')
       ->mail('reroute_email_test', 'test_reroute_email', $test_message['to'], $langcode, $test_message['params']);
-    $this->verbose(t('Test email message values: <pre>@test_message</pre>', array('@test_message' => var_export($test_message, TRUE))));
+    $this->verbose(new FormattableMarkup('Test email message values: <pre>@test_message</pre>', ['@test_message' => var_export($test_message, TRUE)]));
 
-    $mails = $this->drupalGetMails();
+    $mails = $this->getMails();
     $mail = end($mails);
-// Check rerouted email to.
-    $this->assertMail('to', $this->rerouteDestination, format_string('To email address was rerouted to @address.', array('@address' => $this->rerouteDestination)));
+
+    // Check rerouted email to.
+    $this->assertMail('to', $this->rerouteDestination, new FormattableMarkup('To email address was rerouted to @address.', ['@address' => $this->rerouteDestination]));
 
     // Check if original destination email address is in rerouted email body.
-    $this->assertOriginallyTo($mail['body'], 'Found the correct "Originally to" line in the body');
+    $this->assertEmailOriginallyTo();
 
     // Check if test message body is found although provided as a string.
     $this->assertTrue(strpos($mail['body'], $test_message['params']['body']) !== FALSE, 'Email body contains original message body although it was provided as a string.');
@@ -76,13 +81,14 @@ class UnusualMessageFieldsTest extends RerouteEmailTestBase {
     $this->assertRaw(t('A String was detected in the body'), 'Recorded in recent log messages: a String was detected in the body.');
 
     // Test the robustness of the CC and BCC keys in headers.
-    $this->assertTrue($mail['headers']['X-Rerouted-Original-Cc'] == $test_message['params']['headers'][$test_cc_key], format_string('X-Rerouted-Original-Cc is correctly set to @test_cc_address, although Cc header message key provided was: @test_cc_key', array(
+    $this->assertTrue($mail['headers']['X-Rerouted-Original-Cc'] == $test_message['params']['headers'][$test_cc_key], new FormattableMarkup('X-Rerouted-Original-Cc is correctly set to @test_cc_address, although Cc header message key provided was: @test_cc_key', [
       '@test_cc_address' => $test_message['params']['headers'][$test_cc_key],
-      '@test_cc_key' => $test_cc_key
-    )));
-    $this->assertTrue($mail['headers']['X-Rerouted-Original-Bcc'] == $test_message['params']['headers'][$test_bcc_key], format_string('X-Rerouted-Original-Bcc is correctly set to @test_bcc_address, although Bcc header message key provided was: @test_bcc_key', array(
+      '@test_cc_key' => $test_cc_key,
+    ]));
+    $this->assertTrue($mail['headers']['X-Rerouted-Original-Bcc'] == $test_message['params']['headers'][$test_bcc_key], new FormattableMarkup('X-Rerouted-Original-Bcc is correctly set to @test_bcc_address, although Bcc header message key provided was: @test_bcc_key', [
       '@test_bcc_address' => $test_message['params']['headers'][$test_bcc_key],
-      '@test_bcc_key' => $test_bcc_key
-    )));
+      '@test_bcc_key' => $test_bcc_key,
+    ]));
   }
+
 }
