93855eeaba8113b0b74d983b0f2db378fdb197e0
Octavian Belu <beluoctavian@gmail.com>
2018-09-25 17:33:05 +0300

    Robo commands for updating and installing the local instance

--- RoboFile.php
+++ RoboFile.php
@@ -9,6 +9,8 @@ use Robo\Robo;
  */
 class RoboFile extends \Robo\Tasks {
 
+  protected $drush = './vendor/bin/drush';
+
   /**
    * Sync DB from production.
    *
@@ -26,8 +28,44 @@ class RoboFile extends \Robo\Tasks {
     $execStack->exec("rm -f $sql_dump $sql_dump_gz");
     $execStack->exec("curl $url --create-dirs -o $sql_dump_gz -u $username:$password");
     $execStack->exec("gzip -d $sql_dump_gz");
-    $execStack->exec("./vendor/bin/drush sql:drop -y");
-    $execStack->exec("./vendor/bin/drush sql:query --file=$sql_dump -y");
+    $execStack->exec("{$this->drush} sql:drop -y");
+    $execStack->exec("{$this->drush} sql:query --file=$sql_dump -y");
     return $execStack->run();
+  }
+
+  /**
+   * Update the local instance.
+   *
+   * @return \Robo\Result
+   * @throws \Robo\Exception\TaskException
+   *
+   * @command site:update
+   */
+  public function siteUpdate() {
+    $execStack = $this->taskExecStack()->stopOnFail();
+    $execStack->exec("{$this->drush} cr");
+    $execStack->exec("{$this->drush} updatedb -y");
+    $execStack->exec("{$this->drush} entup -y");
+    $execStack->exec("{$this->drush} cim sync -y");
+    return $execStack->run();
+  }
+
+  /**
+   * Install the local instance.
+   *
+   * @return bool|null|\Robo\Result
+   * @throws \Robo\Exception\TaskException
+   *
+   * @command site:install
+   */
+  public function siteInstall() {
+    if ($this->sqlSync()->wasSuccessful() && $this->siteUpdate()->wasSuccessful()) {
+      $execStack = $this->taskExecStack()->stopOnFail();
+      $execStack->exec("{$this->drush} user:password drupal@eaudeweb.ro password");
+      $execStack->exec("{$this->drush} cset system.logging error_level verbose -y");
+      $execStack->exec("{$this->drush} cr");
+      return $execStack->run();
+    }
+    return FALSE;
   }
 }
