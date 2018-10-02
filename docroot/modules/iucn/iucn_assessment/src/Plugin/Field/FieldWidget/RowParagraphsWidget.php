b8a1742f033d9907dfe533bdd67a63c850b3725a
stefan.butura <stefan_butura@yahoo.com>
2018-10-02 13:34:23 +0300

    refs #5393 - Refactored the widget.

--- docroot/modules/iucn/iucn_assessment/src/Plugin/Field/FieldWidget/RowParagraphsWidget.php
+++ docroot/modules/iucn/iucn_assessment/src/Plugin/Field/FieldWidget/RowParagraphsWidget.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\iucn_assessment\Plugin\Field\FieldWidget;
 
+use Drupal\Core\Entity\Entity\EntityFormDisplay;
 use Drupal\paragraphs\Plugin\Field\FieldWidget\ParagraphsWidget;
 use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Form\FormStateInterface;
@@ -11,10 +12,7 @@ use Drupal\paragraphs\ParagraphInterface;
 use Drupal\Component\Utility\Unicode;
 
 /**
- * Plugin implementation of the 'entity_reference paragraphs' widget.
- *
- * We hide add / remove buttons when translating to avoid accidental loss of
- * data because these actions effect all languages.
+ * Plugin implementation of the 'row_entity_reference_paragraphs' widget.
  *
  * @FieldWidget(
  *   id = "row_entity_reference_paragraphs",
@@ -28,10 +26,45 @@ use Drupal\Component\Utility\Unicode;
 class RowParagraphsWidget extends ParagraphsWidget {
 
   /**
+   * The last paragraph entity that has been processed.
+   *
+   * @var \Drupal\paragraphs\ParagraphInterface
+   */
+  protected $paragraphsEntity;
+
+  /**
+   * The number of columns in the grid.
+   *
+   * @var int
+   */
+  protected $colCount;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return array(
+      'title' => t('Paragraph'),
+      'title_plural' => t('Paragraphs'),
+      'edit_mode' => 'closed',
+      'closed_mode' => 'summary',
+      'autocollapse' => 'none',
+      'add_mode' => 'dropdown',
+      'form_display_mode' => 'default',
+      'default_paragraph_type' => '',
+      'features' => [],
+    );
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
     $element = parent::formElement($items, $delta, $element, $form, $form_state);
+
+    unset($element['top']['type']);
+    unset($element['top']['icons']);
+    unset($element['top']['summary']);
 
     $field_name = $this->fieldDefinition->getName();
     $parents = $element['#field_parents'];
@@ -41,18 +74,25 @@ class RowParagraphsWidget extends ParagraphsWidget {
     $widget_state = static::getWidgetState($parents, $field_name, $form_state);
 
     $paragraphs_entity = $widget_state['paragraphs'][$delta]['entity'];
-    $summary = $this->getSummaryComponents($paragraphs_entity);
-    $summary_row = [
-      '#type' => 'table',
-      '#header' => array_keys($summary),
-      '#rows' => [$summary],
-      '#attributes' => ['class' => ['table', 'table-responsive']],
-    ];
+    $components = $this->getSummaryComponents($paragraphs_entity);
+    $containers = $this->getSummaryContainers($components);
 
-    unset($element['top']['type']);
-    unset($element['top']['icons']);
+    $element['top']['actions']['#weight'] = 9999;
 
-    $element['top']['summary'] = $summary_row;
+    $element['top']['summary'] = $containers;
+    $count = count($containers) + 1;
+
+    foreach ($components as $key => $component) {
+      if ($component['span'] == 2) {
+        $count++;
+      }
+    }
+
+    $this->colCount = $count;
+
+    $element['top']['#attributes']['class'][] = "paragraph-top-col-$count";
+
+    $this->paragraphsEntity = $paragraphs_entity;
     return $element;
   }
 
@@ -61,74 +101,155 @@ class RowParagraphsWidget extends ParagraphsWidget {
    */
   public function formMultipleElements(FieldItemListInterface $items, array &$form, FormStateInterface $form_state) {
     $elements = parent::formMultipleElements($items, $form, $form_state);
-    $field_name = $this->fieldDefinition->getName();
 
-    $parents = $elements[0]['#field_parents'];
+    if (!empty($this->paragraphsEntity)) {
+      $header_components = $this->getHeaderComponents($this->paragraphsEntity);
+      $header_components += ['actions' => $this->t('Actions')];
+      $header_containers = $this->getSummaryContainers($header_components);
+      $count = $this->colCount;
 
-    /** @var \Drupal\paragraphs\Entity\Paragraph $paragraphs_entity */
-    $paragraphs_entity = NULL;
-    $widget_state = static::getWidgetState($parents, $field_name, $form_state);
-    $paragraphs_entity = $widget_state['paragraphs'][0]['entity'];
-    $components = $this->getSummaryComponents($paragraphs_entity);
-    array_unshift($components, ['xd' => 'xd']);
-    $header = [
-      '#type' => 'table',
-      '#rows' => [],
-      '#header' => array_keys($components),
-    ];
-    array_unshift($elements, $header);
-
+      $header = [
+        'header' => [
+          '#type' => 'container',
+          '#weight' => -2000,
+          '#attributes' => [
+            'class' => [
+              'paragraph-top',
+              'paragraph-header',
+              "paragraph-top-col-$count",
+            ],
+          ],
+          'data' => $header_containers,
+        ],
+      ];
+      $elements += $header;
+    }
     $elements['#attached']['library'][] = 'iucn_assessment/iucn_assessment.row_paragraph';
     return $elements;
   }
 
+  /**
+   * Returns an array containing the components for the header.
+   *
+   * @param \Drupal\paragraphs\ParagraphInterface $paragraph
+   *   The paragraph entity.
+   *
+   * @return array
+   *   The components.
+   */
+  public function getHeaderComponents(ParagraphInterface $paragraph) {
+    $header = [];
+
+    $components = $this->getFieldComponents($paragraph);
+    foreach (array_keys($components) as $field_name) {
+      if (!$paragraph->hasField($field_name)) {
+        continue;
+      }
+      $field_definition = $paragraph->getFieldDefinition($field_name);
+      $header[$field_name]['value'] = $field_definition->getLabel();
+      if ($field_definition->getType() == 'string_long') {
+        $header[$field_name]['span'] = 2;
+      }
+      else {
+        $header[$field_name]['span'] = 1;
+      }
+    }
+
+    $header += [
+      'actions' => [
+        'value' => $this->t('Actions'),
+        'span' => 1,
+      ],
+    ];
+    return $header;
+  }
 
   /**
-   * {@inheritdoc}
+   * Creates the markup for the summary components.
+   *
+   * @param array $components
+   *   The summary components.
+   *
+   * @return array
+   *   The summary markup.
+   */
+  public function getSummaryContainers(array $components) {
+    $containers = [];
+    foreach ($components as $key => $component) {
+      $span = $component['span'];
+      $containers[$key] = [
+        '#type' => 'container',
+        '#attributes' => [
+          'class' => [
+            'paragraph-summary-component',
+            "paragraph-summary-component-$key",
+            "paragraph-summary-component-span-$span",
+          ],
+        ],
+        'data' => ['#markup' => $component['value']],
+      ];
+      if ($key === 'actions') {
+        $containers[$key]['#attributes']['class'] = 'paragraphs-actions';
+      }
+    }
+
+    return $containers;
+  }
+
+  /**
+   * Returns the field components for the default display view of a paragraph.
+   *
+   * @param \Drupal\paragraphs\ParagraphInterface $paragraph
+   *   The paragraph entity.
+   *
+   * @return array
+   *   The field components.
+   */
+  public function getFieldComponents(ParagraphInterface $paragraph) {
+    $bundle = $paragraph->getType();
+    $components = EntityFormDisplay::load("paragraph.$bundle.default")->getComponents();
+    uasort($components, 'Drupal\Component\Utility\SortArray::sortByWeightElement');
+    return $components;
+  }
+
+  /**
+   * Returns an array containing a summary for every field inside a paragraph.
+   *
+   * @param \Drupal\paragraphs\ParagraphInterface $paragraph
+   *   The paragraph entity.
+   *
+   * @return array
+   *   The summary array.
    */
   public function getSummaryComponents(ParagraphInterface $paragraph) {
-    $show_behavior_summary = isset($options['show_behavior_summary']) ? $options['show_behavior_summary'] : TRUE;
-    $paragraph->summaryCount = 0;
     $summary = [];
-    $components = entity_get_form_display('paragraph', $paragraph->getType(), 'default')->getComponents();
-    uasort($components, 'Drupal\Component\Utility\SortArray::sortByWeightElement');
+    $components = $this->getFieldComponents($paragraph);
     foreach (array_keys($components) as $field_name) {
       // Components can be extra fields, check if the field really exists.
       if (!$paragraph->hasField($field_name)) {
         continue;
       }
       $field_definition = $paragraph->getFieldDefinition($field_name);
-      // @todo: Call the field label service in order to get the proper labels for certain cycles.
-      $label = $field_definition->getLabel();
       // We do not add content to the summary from base fields, skip them
       // keeps performance while building the paragraph summary.
       if (!($field_definition instanceof FieldConfigInterface) || !$paragraph->get($field_name)->access('view')) {
         continue;
       }
 
-      if ($field_definition->getType() == 'image' || $field_definition->getType() == 'file') {
-        $file_summary = $paragraph->getFileSummary($field_name);
-        if ($file_summary != '') {
-          $summary[$label] = $file_summary;
-        }
-      }
-
       $text_summary = $this->getTextSummary($paragraph, $field_name, $field_definition);
-      if ($text_summary != '') {
-        $summary[$label] = $text_summary;
+      $summary[$field_name]['value'] = $text_summary;
+
+      if ($field_definition->getType() == 'image' || $field_definition->getType() == 'file') {
+        $summary[$field_name]['value'] = $paragraph->getFileSummary($field_name);
       }
 
       if ($field_definition->getType() == 'entity_reference_revisions') {
-        // Decrease the depth, since we are entering a nested paragraph.
-        $nested_summary = $this->getNestedSummary($paragraph, $field_name);
-        if ($nested_summary != '') {
-          $summary[$label] = $nested_summary;
-        }
+        $summary[$field_name]['value'] = $this->getNestedSummary($paragraph, $field_name);
       }
 
       if ($field_type = $field_definition->getType() == 'entity_reference') {
         if ($paragraph->get($field_name)->entity && $paragraph->get($field_name)->entity->access('view label')) {
-          $summary[$label] = $paragraph->get($field_name)->entity->label();
+          $summary[$field_name]['value'] = $paragraph->get($field_name)->entity->label();
         }
       }
 
@@ -136,7 +257,7 @@ class RowParagraphsWidget extends ParagraphsWidget {
       if ($field_definition->getType() == 'block_field') {
         if (!empty($paragraph->get($field_name)->first())) {
           $block_admin_label = $paragraph->get($field_name)->first()->getBlock()->getPluginDefinition()['admin_label'];
-          $summary[$label] = $block_admin_label;
+          $summary[$field_name]['value'] = $block_admin_label;
         }
       }
 
@@ -144,31 +265,41 @@ class RowParagraphsWidget extends ParagraphsWidget {
         if (!empty($paragraph->get($field_name)->first())) {
           // If title is not set, fallback to the uri.
           if ($title = $paragraph->get($field_name)->title) {
-            $summary[$label] = $title;
+            $summary[$field_name]['value'] = $title;
           }
           else {
-            $summary[$label] = $paragraph->get($field_name)->uri;
+            $summary[$field_name]['value'] = $paragraph->get($field_name)->uri;
           }
         }
       }
-    }
 
-    if ($show_behavior_summary) {
-      $paragraphs_type = $paragraph->getParagraphType();
-      foreach ($paragraphs_type->getEnabledBehaviorPlugins() as $plugin_id => $plugin) {
-        if ($plugin_summary = $plugin->settingsSummary($paragraph)) {
-          $summary = array_merge($summary, $plugin_summary);
-        }
+      if ($field_definition->getType() == 'string_long') {
+        $summary[$field_name]['span'] = 2;
+      }
+      else {
+        $summary[$field_name]['span'] = 1;
       }
     }
 
     foreach ($summary as &$value) {
-      $value = strip_tags($value);
+      $value['value'] = strip_tags($value['value']);
     }
     return $summary;
   }
 
-
+  /**
+   * Returns the summary of a paragraph's text field.
+   *
+   * @param \Drupal\paragraphs\ParagraphInterface $paragraph
+   *   The paragraph entity.
+   * @param string $field_name
+   *   The field name.
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The field definition for the field.
+   *
+   * @return string
+   *   The summary.
+   */
   public function getTextSummary(ParagraphInterface $paragraph, $field_name, FieldDefinitionInterface $field_definition) {
     $text_types = [
       'text_with_summary',
@@ -192,8 +323,8 @@ class RowParagraphsWidget extends ParagraphsWidget {
       }
 
       $text = $paragraph->get($field_name)->value;
-      if (strlen($text) > 150) {
-        $text = Unicode::truncate($text, 150);
+      if (strlen($text) > 600) {
+        $text = Unicode::truncate($text, 600);
         $text .= '...';
       }
       $summary = $text;
@@ -202,6 +333,17 @@ class RowParagraphsWidget extends ParagraphsWidget {
     return trim($summary);
   }
 
+  /**
+   * Returns the text summary of a referenced paragraph field.
+   *
+   * @param \Drupal\paragraphs\ParagraphInterface $paragraph
+   *   The paragraph entity.
+   * @param string $field_name
+   *   The field name.
+   *
+   * @return string
+   *   The text summary.
+   */
   protected function getNestedSummary(ParagraphInterface $paragraph, $field_name) {
     $summary = [];
 
@@ -210,13 +352,12 @@ class RowParagraphsWidget extends ParagraphsWidget {
       if ($entity instanceof ParagraphInterface) {
         $summary_components = $this->getSummaryComponents($entity);
         $first_component = reset($summary_components);
-        $summary[] = $first_component;
-
+        $summary[] = $first_component['value'];
       }
     }
 
     if (empty($summary)) {
-      return NULL;
+      return '';
     }
 
     $paragraph_summary = implode(', ', $summary);
