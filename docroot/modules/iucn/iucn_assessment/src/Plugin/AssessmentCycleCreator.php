0c9f43be7b789ec90add3134d765ab31804987f2
Octavian Belu <beluoctavian@gmail.com>
2018-10-10 17:25:48 +0300

    Refs #5450 - documented the code

--- docroot/modules/iucn/iucn_assessment/src/Plugin/AssessmentCycleCreator.php
+++ docroot/modules/iucn/iucn_assessment/src/Plugin/AssessmentCycleCreator.php
@@ -16,6 +16,7 @@ class AssessmentCycleCreator {
   /** @var \Drupal\Core\Entity\EntityTypeManagerInterface */
   protected $entityTypeManager;
 
+  /** @var \Drupal\node\NodeStorageInterface */
   protected $nodeStorage;
 
   /** @var \Drupal\Core\Entity\EntityFieldManagerInterface */
@@ -27,10 +28,10 @@ class AssessmentCycleCreator {
   /** @var \Drupal\Core\Logger\LoggerChannelInterface */
   protected $logger;
 
-  /** @var array */
+  /** @var int[] */
   protected $availableCycles = [];
 
-  /** @var array|\Drupal\Core\Field\FieldDefinitionInterface[] */
+  /** @var \Drupal\Core\Field\FieldDefinitionInterface[] */
   protected $siteAssessmentFields = [];
 
   public function __construct(EntityTypeManagerInterface $entityTypeManager, EntityFieldManagerInterface $entityFieldManager, StateInterface $state, LoggerChannelFactoryInterface $loggerChannelFactory) {
@@ -45,12 +46,15 @@ class AssessmentCycleCreator {
   }
 
   /**
-   * @param $cycle
-   * @param string $originalCycle
+   * Create site assessments for a new cycle by duplicating the ones from an
+   * older cycle.
+   *
+   * @param int $cycle
+   * @param int $originalCycle
    *
    * @throws \Exception
    */
-  public function createAssessments($cycle, $originalCycle = '2017') {
+  public function createAssessments($cycle, $originalCycle = 2017) {
     if (!array_key_exists($cycle, $this->availableCycles) || !array_key_exists($originalCycle, $this->availableCycles)) {
       throw new \InvalidArgumentException('Invalid cycle parameter. Available cycles: ' . implode(', ', array_keys($this->availableCycles)));
     }
