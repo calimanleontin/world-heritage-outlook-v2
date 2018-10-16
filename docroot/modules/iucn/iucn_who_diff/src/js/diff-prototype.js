6ef946e5eba2fd5181717d2d10137eb0208719e9
marapredescu1 <mara.predescu@eaudeweb.ro>
2018-10-16 19:26:05 +0300

    Issue #5414 added a benefits row

--- docroot/modules/iucn/iucn_who_diff/src/js/diff-prototype.js
+++ docroot/modules/iucn/iucn_who_diff/src/js/diff-prototype.js
@@ -1,40 +1,40 @@
 (function($) {
 
-  $( '[data-name]' ).on( 'click', '[data-copy]', ( e ) => {
-    $eventTarget = $( e.target );
-    $delegateTarget = $( e.delegateTarget );
-    $name = $delegateTarget.data('name');
+  // $( '[data-name]' ).on( 'click', '[data-copy]', ( e ) => {
+  //   $eventTarget = $( e.target );
+  //   $delegateTarget = $( e.delegateTarget );
+  //   $name = $delegateTarget.data('name');
 
-    console.log($name, $delegateTarget.find('.diff-addedline > .diff-content').html());
+  //   console.log($name, $delegateTarget.find('.diff-addedline > .diff-content').html());
 
-    $targetValue = $delegateTarget.find('.diff-addedline > .diff-content').html();
+  //   $targetValue = $delegateTarget.find('.diff-addedline > .diff-content').html();
 
 
-    $target = $('#final-version')
-                .find('[data-name="' + $name + '"]');
-    $target.find('textarea').val($targetValue);
-    // $target.find('.chosen-enable').val($targetValue);
-    // $target.html($targetValue);
-    // console.log(Drupal.behaviors.chosen.getElements());
+  //   $target = $('#final-version')
+  //               .find('[data-name="' + $name + '"]');
+  //   $target.find('textarea').val($targetValue);
+  //   // $target.find('.chosen-enable').val($targetValue);
+  //   // $target.html($targetValue);
+  //   // console.log(Drupal.behaviors.chosen.getElements());
 
-  });
+  // });
 
-  $( '[data-name]' ).on( 'click', '[data-initial]', ( e ) => {
-    $eventTarget = $( e.target );
-    $delegateTarget = $( e.delegateTarget );
-    $name = $delegateTarget.data('name');
+  // $( '[data-name]' ).on( 'click', '[data-initial]', ( e ) => {
+  //   $eventTarget = $( e.target );
+  //   $delegateTarget = $( e.delegateTarget );
+  //   $name = $delegateTarget.data('name');
 
-    $initialTargetValue = $('#initial-version')
-                            .find('[data-name="' + $name + '"]').html();
+  //   $initialTargetValue = $('#initial-version')
+  //                           .find('[data-name="' + $name + '"]').html();
 
-    $target = $('#final-version')
-                .find('[data-name="' + $name + '"]');
-    $target.find('textarea').val($initialTargetValue.trim());
-    // $target.find('.chosen-enable').val($targetValue);
-    // $target.html($targetValue);
-    // console.log(Drupal.behaviors.chosen.getElements());
+  //   $target = $('#final-version')
+  //               .find('[data-name="' + $name + '"]');
+  //   $target.find('textarea').val($initialTargetValue.trim());
+  //   // $target.find('.chosen-enable').val($targetValue);
+  //   // $target.html($targetValue);
+  //   // console.log(Drupal.behaviors.chosen.getElements());
 
-  });
+  // });
 
 
 }(jQuery));
