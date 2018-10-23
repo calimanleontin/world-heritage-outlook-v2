1586bdf0281a88f68aee5baf8a98abee0539e196
stefan.butura <stefan_butura@yahoo.com>
2018-10-23 15:54:53 +0300

    refs #5411 - Current state now showing up on the node edit page.

--- docroot/modules/iucn/iucn_assessment/src/Form/NodeSiteAssessmentForm.php
+++ docroot/modules/iucn/iucn_assessment/src/Form/NodeSiteAssessmentForm.php
@@ -3,10 +3,11 @@
 namespace Drupal\iucn_assessment\Form;
 
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\workflow\Entity\WorkflowState;
 
 class NodeSiteAssessmentForm {
 
-  public static function alter(&$form, FormStateInterface $form_state) {
+  public static function alter(&$form, FormStateInterface $form_state, $form_id) {
     foreach (['status', 'revision_log', 'revision_information', 'revision'] as $item) {
       $form[$item]['#access'] = FALSE;
     }
@@ -15,5 +16,23 @@ class NodeSiteAssessmentForm {
     // be created in hook_node_presave().
     $form['revision']['#default_value'] = FALSE;
     $form['revision']['#disabled'] = FALSE;
+
+    // Add the current state on the node edit page.
+    if ($form_id == 'node_site_assessment_edit_form') {
+      $node = $form_state->getFormObject()->getEntity();
+      $current_state = $node->field_state->value;
+      if (!empty($current_state)) {
+        $state_entity = WorkflowState::load($current_state);
+      }
+      else {
+        $state_entity = NULL;
+      }
+      $state_label = !empty($state_entity) ? $state_entity->label() : 'Creation';
+      $form['current_state'] = [
+        '#weight' => -9999,
+        '#type' => 'markup',
+        '#markup' => t('Current state: <b>@state</b>', ['@state' => $state_label]),
+      ];
+    }
   }
 }
