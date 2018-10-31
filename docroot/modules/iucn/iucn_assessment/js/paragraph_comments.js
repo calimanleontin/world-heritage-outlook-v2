d33a9d1a28b0f877bed462016ccd35538b30de0d
stefan.butura <stefan_butura@yahoo.com>
2018-10-31 12:23:29 +0200

    refs #5464 - finished the bubble.

--- docroot/modules/iucn/iucn_assessment/js/paragraph_comments.js
+++ docroot/modules/iucn/iucn_assessment/js/paragraph_comments.js
@@ -14,30 +14,48 @@
       var selected_html = selected.html();
       selected_html += '<i class="fa fa-comment-dots"></i>';
       selected.html(selected_html);
+      var comments_textarea = $(context).find('.paragraph-comments-textarea');
+      var comment_dots = $(context).find(".fa-comment-dots");
 
       function deselect(e) {
-        $(context).find('.paragraph-comments-textarea').slideFadeToggle(function() {
+        comments_textarea.slideFadeToggle(function() {
           e.removeClass('selected');
         });
+      }
+
+      function moveRelativeTo(a, b) {
+        var destination = b.offset();
+        var document_width = $(window).width();
+        var comments_location;
+        if (destination.left + 25 > document_width / 2) {
+          comments_location = {top: destination.top + 25, right: document_width - destination.left - 25, left: 'initial'};
+        }
+        else {
+          comments_location = {top: destination.top + 25, left: destination.left, right: 'initial'};
+        }
+        a.css(comments_location);
       }
 
       $.fn.slideFadeToggle = function(easing, callback) {
         return this.animate({ opacity: 'toggle', height: 'toggle' }, 'fast', easing, callback);
       };
 
-      $(context).find(".fa-comment-dots").click(function (e) {
+      comment_dots.click(function (e) {
         e.preventDefault();
-        if($(this).hasClass('selected')) {
-          deselect($(this));
-        }
-        else {
+        if (!$(this).hasClass('selected')) {
           $(this).addClass('selected');
-          var textarea = $(context).find('.paragraph-comments-textarea');
-          textarea.slideFadeToggle();
-          var destination = $(this).offset();
-          textarea.css({top: destination.top + 25});
+          comments_textarea.slideFadeToggle();
+          moveRelativeTo(comments_textarea, $(this));
         }
-        return false;
+      });
+
+      $(document).mouseup(function(e) {
+        // if the target of the click isn't the container nor a descendant of the container
+        if (!comments_textarea.is(e.target) && comments_textarea.has(e.target).length === 0) {
+          if (comment_dots.hasClass('selected')) {
+            deselect(comment_dots);
+          }
+        }
       });
     }
   };
