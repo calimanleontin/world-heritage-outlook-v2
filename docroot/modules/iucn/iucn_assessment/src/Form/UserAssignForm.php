9142bbffe0b89e2dea5254ba3c26bc9e4ccc580a
Octavian Belu <beluoctavian@gmail.com>
2018-10-31 14:36:33 +0200

    Refs #5538 - retrive unassigned assessments

--- docroot/modules/iucn/iucn_assessment/src/Form/UserAssignForm.php
+++ docroot/modules/iucn/iucn_assessment/src/Form/UserAssignForm.php
@@ -3,6 +3,7 @@
 namespace Drupal\iucn_assessment\Form;
 
 use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Session\AccountInterface;
@@ -30,18 +31,23 @@ class UserAssignForm extends FormBase {
    */
   protected $iucnAssessmentWorkflow;
 
+  /** @var \Drupal\node\NodeStorageInterface */
+  protected $nodeStorage;
+
   /**
    * Constructs a new UserAssignForm object.
    */
-  public function __construct(AccountProxyInterface $currentUser, AssessmentWorkflow $iucnAssessmentWorkflow) {
+  public function __construct(AccountProxyInterface $currentUser, AssessmentWorkflow $iucnAssessmentWorkflow, EntityTypeManagerInterface $entityTypeManager) {
     $this->currentUser = $currentUser;
     $this->iucnAssessmentWorkflow = $iucnAssessmentWorkflow;
+    $this->nodeStorage = $entityTypeManager->getStorage('node');
   }
 
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('current_user'),
-      $container->get('iucn_assessment.workflow')
+      $container->get('iucn_assessment.workflow'),
+      $container->get('entity_type.manager')
     );
   }
 
@@ -71,30 +77,71 @@ class UserAssignForm extends FormBase {
       $this->messenger()->addError('This user cannot be assigned to any site.');
       return;
     }
-    $roles = array_combine($roles, $roles);
+    $roles = [NULL => t('- Select -')] + array_combine($roles, $roles);
     $form = [
       '#title' => t('Assign %user to multiple sites', ['%user' => $user->getAccountName()]),
-    ];
-    $form['role'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Role'),
-      '#multiple' => FALSE,
-      '#options' => $roles,
-      '#weight' => 0,
-    ];
-    $form['sites'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Sites'),
-      '#multiple' => TRUE,
-      '#options' => [],
-      '#weight' => 1,
-    ];
-    $form['assign'] = [
-      '#type' => 'submit',
-      '#value' => $this->t('Assign'),
-      '#weight' => 2,
+      'role' => [
+        '#type' => 'select',
+        '#title' => $this->t('Role'),
+        '#multiple' => FALSE,
+        '#required' => TRUE,
+        '#options' => $roles,
+        '#ajax' => [
+          'callback' => '::roleAjaxCallback',
+          'wrapper' => 'sites-container',
+        ],
+
+      ],
+      'sites' => [
+        '#type' => 'select',
+        '#title' => $this->t('Sites'),
+        '#multiple' => TRUE,
+        '#required' => TRUE,
+        '#options' => [],
+        '#chosen' => TRUE,
+        '#prefix' => '<div id="sites-container">',
+        '#suffix' => '</div>',
+      ],
+      'submit' => [
+        '#type' => 'submit',
+        '#value' => $this->t('Assign'),
+      ],
     ];
     return $form;
+  }
+
+  public function roleAjaxCallback(array $form, FormStateInterface $form_state) {
+    $role = $form_state->getValue('role');
+    if (empty($role)) {
+      $form['sites']['#options'] = [];
+      return $form['sites'];
+    }
+
+    $states = [
+      'assessment_creation',
+      'assessment_new',
+      'assessment_under_evaluation',
+      'assessment_under_assessment',
+      'assessment_ready_for_review',
+    ];
+    $query = $this->nodeStorage->getQuery()
+      ->condition('type', 'site_assessment')
+      ->condition('field_state', $states, 'IN')
+      ->notExists(($role == 'reviewer') ? "field_{$role}s" : "field_{$role}");
+    $ids = $query->execute();
+    if (empty($ids)) {
+      $this->messenger()->addError(t('There are no assessments to which the user can be assigned.'));
+      return $form['sites'];
+    }
+
+    /** @var \Drupal\node\NodeInterface[] $assessments */
+    $assessments = $this->nodeStorage->loadMultiple($ids);
+    $options = [];
+    foreach ($assessments as $assessment) {
+      $options[$assessment->id()] = $assessment->getTitle();
+    }
+    $form['sites']['#options'] = $options;
+    return $form['sites'];
   }
 
   /**
