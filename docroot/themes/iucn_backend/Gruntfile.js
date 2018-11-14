3f9aae41042f52bb15d596bc3ec992460b467a6d
Octavian Belu <beluoctavian@gmail.com>
2018-11-14 12:37:26 +0200

    Refactored backend theme css

---
+++ docroot/themes/iucn_backend/Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.initConfig({
+        watch: {
+            files: ["less/**/*.less"],
+            tasks: ["less"]
+        },
+        less: {
+            development: {
+                options: {
+                    outputSourceFiles: true,
+                    sourceMap: true,
+                    outputSourceFiles: true,
+                    paths: ["less/"]
+                },
+                files: {
+                    "css/style.css": "less/style.less"
+                }
+            }
+        }
+    });
+    grunt.registerTask('default', ['less', 'watch']);
+};
