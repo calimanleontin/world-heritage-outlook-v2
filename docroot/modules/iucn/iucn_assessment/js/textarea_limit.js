ee3ddc4d2508dc9a349d97f80e764cd28915c910
Octavian Belu <beluoctavian@gmail.com>
2018-11-20 11:38:08 +0200

    Refs #5556 - jquery-limit library + under work textarea limit

---
+++ docroot/modules/iucn/iucn_assessment/js/textarea_limit.js
@@ -0,0 +1,16 @@
+(function($, Drupal, drupalSettings) {
+  Drupal.behaviors.textarea_limit = {
+    attach: function(context, settings) {
+      // $('.character-limited').each(function() {
+      //   var limit = $(this).find('.limit-count-number').data('limit');
+      //   console.log(limit);
+      //
+      //   $(this).find('textarea').each(function() {
+      //     var id = $(this).parents('.form-type-textarea').parent().find('.limit-text').find('.limit-count-number').attr('id');
+      //
+      //     $(this).limit(limit, '#' + id);
+      //   });
+      // });
+    }
+  }
+})(jQuery, Drupal, drupalSettings);
