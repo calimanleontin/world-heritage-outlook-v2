94262ce86493cd8a57f1bf8fc0e54fc52928271e
stefan.butura <stefan_butura@yahoo.com>
2018-12-11 13:56:36 +0200

    Adding paragraphs with modal form now correctly sets parent entity.

--- docroot/modules/iucn/iucn_assessment/src/Form/IucnModalParagraphAddForm.php
+++ docroot/modules/iucn/iucn_assessment/src/Form/IucnModalParagraphAddForm.php
@@ -12,16 +12,20 @@ class IucnModalParagraphAddForm extends IucnModalForm {
   public function save(array $form, FormStateInterface $form_state) {
     $route_match = $this->getRouteMatch();
     $temporary_data = $form_state->getTemporary();
+    $field = $route_match->getParameter('field');
 
     $node_revision = isset($temporary_data['node_revision']) ?
       $temporary_data['node_revision'] :
       $route_match->getParameter('node_revision');
-    $this->entity->save();
 
     $parent_entity_revision = $this->entityTypeManager
       ->getStorage('node')
       ->loadRevision($node_revision);
-    $this->insertParagraph($parent_entity_revision);
+
+    $this->entity->setParentEntity($parent_entity_revision, $field);
+    $this->entity->save();
+
+    $this->insertParagraph($parent_entity_revision, $field);
 
     $save_status = $parent_entity_revision->save();
 
@@ -33,9 +37,8 @@ class IucnModalParagraphAddForm extends IucnModalForm {
   /**
    * Insert the value into the ItemList either before or after.
    */
-  protected function insertParagraph($parent_entity) {
+  protected function insertParagraph($parent_entity, $field) {
     $route_match = $this->getRouteMatch();
-    $field = $route_match->getParameter('field');
     $value = [
       'target_id' => $this->entity->id(),
       'target_revision_id' => $this->entity->getRevisionId(),
