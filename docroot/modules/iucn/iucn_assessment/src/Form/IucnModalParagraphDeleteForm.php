9813e63f0cb389fbbac80fa3d770497d6163dcaf
stefan.butura <stefan_butura@yahoo.com>
2018-12-19 12:48:28 +0200

    refs #5752 - values cannot be deleted if there is a threat that only references them.

--- docroot/modules/iucn/iucn_assessment/src/Form/IucnModalParagraphDeleteForm.php
+++ docroot/modules/iucn/iucn_assessment/src/Form/IucnModalParagraphDeleteForm.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\iucn_assessment\Form;
 
+use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormState;
 use Drupal\Core\Form\FormStateInterface;
@@ -10,6 +11,16 @@ use Drupal\linkit\ProfileInterface;
 use Drupal\paragraphs\ParagraphInterface;
 
 class IucnModalParagraphDeleteForm extends FormBase {
+
+  /**
+   * @var EntityInterface
+   */
+  protected $parent_entity;
+
+  /**
+   * @var ParagraphInterface
+   */
+  protected $paragraph;
 
   /**
    * {@inheritdoc}
@@ -26,25 +37,72 @@ class IucnModalParagraphDeleteForm extends FormBase {
 
     $form['actions'] = ['#type' => 'actions'];
 
-    $form['warning'] = [
-      '#type' => 'markup',
-      '#markup' => '<div class="delete-warning">' . $this->t('Are you sure you want to delete this row? This action cannot be reverted.') . '</div>',
-    ];
+    $this->paragraph = $this->getRouteMatch()->getParameter('paragraph_revision');
+    $this->parent_entity = $this->getRouteMatch()->getParameter('node_revision');
 
-    $form['actions']['submit'] = [
-      '#type' => 'submit',
-      '#value' => $this->t('Delete'),
-      '#ajax' => [
-        'callback' => '::ajaxDelete',
-        'event' => 'click',
-        'progress' => [
-          'type' => 'throbber',
-          'message' => NULL,
+    $can_delete = TRUE;
+    $blocking_threats = [];
+    // Values cannot be deleted if there is at least one threat
+    // with exactly one referenced value - this paragraph.
+    if (in_array($this->paragraph->bundle(), ['as_site_value_wh', 'as_site_value_bio'])) {
+      $threats = array_merge($this->parent_entity->get('field_as_threats_current')->getValue(), $this->parent_entity->get('field_as_threats_potential')->getValue());
+      $paragraph_storage = \Drupal::entityTypeManager()->getStorage('paragraph');
+      foreach ($threats as $threat) {
+        /** @var \Drupal\paragraphs\ParagraphInterface $threat_paragraph */
+        $threat_paragraph = $paragraph_storage->loadRevision($threat['target_revision_id']);
+        $affected_values = array_merge($threat_paragraph->get('field_as_threats_values_wh')->getValue(), $threat_paragraph->get('field_as_threats_values_bio')->getValue());
+        if (count($affected_values) != 1) {
+          continue;
+        }
+        $affected_value = $affected_values[0]['target_revision_id'];
+        if ($affected_value == $this->paragraph->getRevisionId()) {
+          $blocking_threats[] = $threat_paragraph->field_as_threats_threat->value;
+          $can_delete = FALSE;
+        }
+      }
+    }
+
+    if ($can_delete) {
+      $form['warning'] = [
+        '#type' => 'markup',
+        '#markup' => '<div class="delete-warning">' . $this->t('Are you sure you want to delete this row? This action cannot be reverted.') . '</div>',
+      ];
+
+      $form['actions']['submit'] = [
+        '#type' => 'submit',
+        '#value' => $this->t('Delete'),
+        '#ajax' => [
+          'callback' => '::ajaxDelete',
+          'event' => 'click',
+          'progress' => [
+            'type' => 'throbber',
+            'message' => NULL,
+          ],
+          'disable-refocus' => TRUE,
         ],
-        'disable-refocus' => TRUE,
-      ],
-      '#attributes' => ['class' => ['button--primary']],
-    ];
+        '#attributes' => ['class' => ['button--primary']],
+      ];
+    }
+    else {
+      $warning = $this->t('This value cannot be deleted because it is the only affected value for the some threats. Please edit or delete these threats first:');
+      $form['warning'] = [
+        '#type' => 'markup',
+        '#markup' => '<div class="delete-warning">' . $warning . '</div>',
+      ];
+      $form['threats'] = [
+        '#items' => $blocking_threats,
+        '#theme' => 'item_list',
+        '#list_type' => 'ul',
+      ];
+      $form['actions']['go_to_threats'] = [
+        '#type' => 'link',
+        '#title' => $this->t('Go to threats'),
+        '#url' => $this->parent_entity->toUrl('edit-form', ['query' => ['tab' => 'threats']]),
+        '#attributes' => [
+          'class' => ['button', 'button--primary'],
+        ],
+      ];
+    }
 
     IucnModalForm::buildCancelButton($form);
 
@@ -52,16 +110,12 @@ class IucnModalParagraphDeleteForm extends FormBase {
   }
 
   public function ajaxDelete(&$form, FormStateInterface $form_state) {
-    $route_match = $this->getRouteMatch();
-    /** @var ParagraphInterface $paragraph */
-    $paragraph = $route_match->getParameter('paragraph_revision');
-    $field = $route_match->getParameter('field');
-    $parent_entity = $route_match->getParameter('node_revision');
+    $field = $this->getRouteMatch()->getParameter('field');
 
-    $field_values = $parent_entity->get($field)->getValue();
-    $key = array_search($paragraph->id(), array_column($field_values, 'target_id'));
-    $parent_entity->get($field)->removeItem($key);
-    $parent_entity->save();
+    $field_values = $this->parent_entity->get($field)->getValue();
+    $key = array_search($this->paragraph->id(), array_column($field_values, 'target_id'));
+    $this->parent_entity->get($field)->removeItem($key);
+    $this->parent_entity->save();
     return IucnModalForm::assessmentAjaxSave($form, $form_state);
   }
 
