5ca41f80dcef48cbb36e627cdb6b4d8a860abb28
stefan.butura <stefan_butura@yahoo.com>
2018-12-21 12:49:20 +0200

    refs #5755 - programatically deselecting a subcategory checkbox now triggers change event.

--- docroot/modules/iucn/iucn_assessment/js/as_option_buttons.js
+++ docroot/modules/iucn/iucn_assessment/js/as_option_buttons.js
@@ -9,10 +9,22 @@
 
   Drupal.behaviors.AsOptionsButtonsWidget = {
     attach: function (context) {
+      setTimeout(function () {
+        var $items = $('.as-checkboxes .form-checkbox', context);
+        $items.each(function () {
+          var $parent = $(this).parent();
+          if ($parent.is(":hidden")) {
+            $(this).prop('checked', false);
+            $(this).trigger('change');
+          }
+        });
+      });
+
       $('.options-groups', context).change(function() {
         var $items = $('.as-checkboxes .form-checkbox', context);
         $items.each(function() {
           $(this).prop('checked', false);
+          $(this).trigger('change');
         });
       })
     },
