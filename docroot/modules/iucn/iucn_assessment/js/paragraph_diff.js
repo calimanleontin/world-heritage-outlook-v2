f98c07368258be3791c49445f91e76d55a3d86ff
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2019-01-01 13:55:28 +0200

    5759 Under comparison - see differences modal - display widgets to help the coordinator to accept changes with ease

--- docroot/modules/iucn/iucn_assessment/js/paragraph_diff.js
+++ docroot/modules/iucn/iucn_assessment/js/paragraph_diff.js
@@ -9,45 +9,53 @@
     Drupal.behaviors.paragraphDiff = {
         attach: function (context) {
             $('a.diff-button').on('click', function() {
-                var key = $(this).data('key');
-                var data_selector = '[data-drupal-selector="' + $(this).data('selector') + '"]';
                 var type = $(this).data('type');
-                update(key, data_selector, type);
+                var key = $(this).data('key');
+                var values = drupalSettings.diff[key];
+                var selector = $(this).data('selector');
+                update(values, selector, type);
+
                 var key2 = $(this).data('key2');
                 if (key2) {
-                    var data_selector2 = '[data-drupal-selector="' + $(this).data('selector2') + '"]';
-                    update(key2, data_selector2, type);
+                    var values2 = drupalSettings.diff[key2];
+                    var selector2 = $(this).data('selector2');
+                    update(values2, selector2, type);
                 }
             });
 
-            function update(key, data_selector, type) {
+            function get_data_selector(selector) {
+                return '[data-drupal-selector="' + selector + '"]';
+            }
+
+            function update(values, selector, type) {
+
                 if (type == 'checkboxes') {
 
-                    var values = drupalSettings.diff[key];
-                    $(data_selector + ' input.form-checkbox').prop('checked', false);
+                    $(get_data_selector(selector) + ' input.form-checkbox').prop('checked', false);
                     for (var i = 0; i < values.length; i++) {
-                        $('[data-drupal-selector="' + $(this).data('selector') + '-' + values[i] + '"]').prop('checked', true);
+                        var sel = '[data-drupal-selector="' + selector + '-' + values[i] + '"]';
+                        $(sel).prop('checked', true);
                     }
 
                 } else if (type == 'checkbox') {
 
-                    if (drupalSettings.diff[key]) {
-                        $(data_selector).prop('checked', true);
+                    if (values) {
+                        $(get_data_selector(selector)).prop('checked', true);
                     }
                     else {
-                        $(data_selector).prop('checked', false);
+                        $(get_data_selector(selector)).prop('checked', false);
                     }
 
                 } else if (type == 'select') {
-                    if (drupalSettings.diff[key].length) {
-                        $(data_selector).val(drupalSettings.diff[key]);
+                    if (values.length) {
+                        $(get_data_selector(selector)).val(values);
                     } else {
-                        $(data_selector).val(['_none']);
+                        $(get_data_selector(selector)).val(['_none']);
                     }
 
                 } else {
 
-                    $(data_selector).val(drupalSettings.diff[key]);
+                    $(get_data_selector(selector)).val(values);
 
                 }
 
