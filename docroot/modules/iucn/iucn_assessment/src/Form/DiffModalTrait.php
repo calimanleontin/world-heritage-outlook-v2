a543ef08b26fb861a0b21b6d563a9380d08d1b8a
Tavi Belu <beluoctavian@gmail.com>
2019-01-16 19:13:17 +0200

    Refs #5825 - moved methods which are only used by the diff modals to DiffModalTrait

--- docroot/modules/iucn/iucn_assessment/src/Form/DiffModalTrait.php
+++ docroot/modules/iucn/iucn_assessment/src/Form/DiffModalTrait.php
@@ -44,4 +44,137 @@ trait DiffModalTrait {
     $table[$key]['author'] = $this->getTableCellMarkup($markup, $class, $span, $weight);
   }
 
+  /**
+   * @param $vid
+   *  Assessment node revision id.
+   * @param $fieldType
+   *  Type of field (select, checkboxes, etc).
+   * @param $fieldName
+   *  The machine name of the field.
+   * @param $fieldValue
+   *  The field value which will be copied to the final version.
+   * @param $extraFieldName
+   *  The machine name of the extra field. Some field can be grouped and should use
+   * the same copy button.
+   * @param $extraFieldValue
+   *  The value of the extra field which will be copied to the final version.
+   *
+   * @return
+   *  The rendered element.
+   */
+  public function getCopyValueButton($vid, $fieldType, $fieldName, $fieldValue, $extraFieldName = NULL, $extraFieldValue = NULL) {
+    $key = "{$fieldName}_{$vid}";
+
+    $element = [
+      '#theme' => 'assessment_diff_copy_button',
+      '#type' => $fieldType,
+      '#key' => $key,
+      '#selector' => $this->getJsSelector($fieldName, $fieldType),
+      '#attached' => [
+        'drupalSettings' => [
+          'diff' => [
+            $key => $this->getCopyFieldValue($fieldValue),
+          ],
+        ],
+      ],
+    ];
+
+    if (!empty($extraFieldName)) {
+      $key2 = "{$extraFieldName}_{$vid}";
+      $element['#key2'] = $key2;
+      $element['#selector2'] = $this->getJsSelector($extraFieldName, $fieldType);
+      if (!empty($extraFieldValue)) {
+        $element['#attached']['drupalSettings']['diff'][$key2] = $this->getCopyFieldValue($extraFieldValue);
+      }
+    }
+
+    return render($element);
+  }
+
+  /**
+   * Retrieves the field type from the form element widget.
+   *
+   * @param $widget
+   *  The widget of the field.
+   *
+   * @return string
+   *  The field type.
+   */
+  public function getDiffFieldType($widget) {
+    $type = '';
+    if (!empty($widget['#type'])) {
+      $type = $widget['#type'];
+    }
+    elseif (!empty($widget['value']['#type'])) {
+      $type = $widget['value']['#type'];
+    }
+    elseif (!empty($widget[0]['value']['#type'])) {
+      $type = $widget[0]['value']['#type'];
+    }
+    elseif (!empty($widget[0]['#entity_type'])) {
+      $type = $widget[0]['#entity_type'];
+    }
+    return $type;
+  }
+
+  /**
+   * Builds a selector which can be used in javascript to retrieve field input.
+   *
+   * @param $fieldName
+   *  The machine name of the field.
+   * @param $fieldType
+   *  Type of field (select, checkboxes, etc).
+   *
+   * @return string
+   *  The JS selector.
+   */
+  public function getJsSelector($fieldName, $fieldType) {
+    $selector = 'edit-' . str_replace('_', '-', $fieldName);
+    switch ($fieldType) {
+      case "textarea":
+        $selector .= '-0-value';
+        break;
+
+      case "textfield":
+        $selector .= '-0-value';
+        break;
+
+      case "checkboxes":
+        break;
+
+      case "checkbox":
+        $selector .= '-value';
+        break;
+
+      case "select":
+        break;
+
+      case "paragraph":
+        $selector .= '-select';
+        break;
+    }
+    return $selector;
+  }
+
+  /**
+   * Retrieves the value which will be passed to JS settings.
+   *
+   * @param $fieldValue
+   *  The Drupal-style value of the field.
+   *
+   * @return array|mixed
+   *  The value which will be used by the JS script.
+   */
+  public function getCopyFieldValue($fieldValue) {
+    $value = [];
+    foreach ($fieldValue as $value) {
+      // todo check target_revision_id
+      $value[] = !empty($value['value']) ? $value['value'] : $value['target_id'];
+    }
+    if (count($value) == 1) {
+      $value = reset($value);
+    }
+    return $value;
+  }
+
 }
