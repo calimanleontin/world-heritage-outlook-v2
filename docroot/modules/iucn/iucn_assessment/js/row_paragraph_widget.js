586a8c8e4fd1809e55ffb6b55ef7531d75713d76
marapredescu1 <mara.predescu@eaudeweb.ro>
2019-02-07 18:02:51 +0200

    Issue #5900 assessment tables scrollable on narrower screens

--- docroot/modules/iucn/iucn_assessment/js/row_paragraph_widget.js
+++ docroot/modules/iucn/iucn_assessment/js/row_paragraph_widget.js
@@ -47,4 +47,68 @@
     },
 };
 
+  // Drupal.behaviors.rowParagraphFixedActions = {
+  //   attach: function(context, settings) {
+  //     $(function() {
+  //       var inheritParentDims = function() {
+  //           var $items = $('.field--widget-row-entity-reference-paragraphs .paragraphs-actions', context);
+  //           $items.each(function() {
+  //               var $parent = $(this).parent();
+  //               var $parentHeight = $parent.height();
+  //               var $parentWidth = $parent.width();
+  //               if ($parentWidth === 0) {
+  //                 $parentWidth = 120;
+  //               }
+  //               if ($parentHeight === 0) {
+  //                 $parentHeight = 60;
+  //               }
+  //               $(this).height($parentHeight);
+  //               $(this).width($parentWidth);
+  //               $parent.css('min-height', $parentHeight);
+  //               $parent.addClass('processed');
+  //           });
+  //       };
+
+  //       inheritParentDims();
+  //       $(document).once('upd\ateParagraphActions').on('DOMSubtreeModified', _.debounce(inheritParentDims, 100));
+  //       $(window).once("bind-to-window").on('resize', _.debounce(inheritParentDims, 100));
+  //     });
+  //   }
+  // };
+
+  Drupal.behaviors.scrollAtStart = {
+    attach: function (context, settings) {
+      $(function() {
+        $('.responsive-wrapper', context).scrollLeft(0);
+      });
+    }
+  };
+
+  Drupal.behaviors.doubleScrollBar = {
+    attach: function (context, settings) {
+        $(function(){
+            $(".responsive-wrapper", context).each(function() {
+                var $table = $(this).find('.field-multiple-table');
+                $(this).siblings(".double-scrollbar-helper").find('.inner').width($table.width());
+            });
+
+            $(".responsive-wrapper", context).scroll(function(){
+                $(this).siblings(".double-scrollbar-helper")
+                    .scrollLeft($(this).scrollLeft());
+            });
+            $(".double-scrollbar-helper", context).scroll(function(){
+                $(this).siblings(".responsive-wrapper")
+                    .scrollLeft($(this).scrollLeft());
+            });
+
+            $(window).once("bind-dsb-to-window").on('resize', _.debounce(function(){
+                $(".responsive-wrapper", context).each(function() {
+                    var $table = $(this).find('.field-multiple-table');
+                    $(this).siblings(".double-scrollbar-helper").find('.inner').width($table.width());
+                });
+            }, 100));
+        });
+    }
+  };
+
 })(jQuery, Drupal, _);
