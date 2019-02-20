175cd870fa04a98bd881d10ac7757fa6f0245309
Tavi Belu <beluoctavian@gmail.com>
2019-02-20 13:51:32 +0200

    Refs #6058 - moved each phase in different class because of out of memory issues

---
+++ docroot/modules/iucn/iucn_assessment/src/Tests/Workflow/UnderEvaluationPhaseTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\iucn_assessment\Tests\Workflow;
+
+use Drupal\Core\Url;
+use Drupal\iucn_assessment\Plugin\AssessmentWorkflow;
+use Drupal\iucn_assessment\Tests\TestSupport;
+
+/**
+ * @group iucn_workflow
+ */
+class UnderEvaluationPhaseTest extends WorkflowTest {
+
+  public function testUnderEvaluationPhase() {
+    $assessment = TestSupport::createAssessment();
+    TestSupport::populateAllFieldsData($assessment, 1);
+    $assessment->save();
+    $editUrl = $assessment->toUrl('edit-form');
+    $stateChangeUrl = Url::fromRoute('iucn_assessment.node.state_change', ['node' => $assessment->id()]);
+
+    $this->userLogIn(TestSupport::COORDINATOR1);
+    $this->drupalPostForm($stateChangeUrl, [], static::TRANSITION_LABELS[AssessmentWorkflow::STATUS_UNDER_EVALUATION]);
+
+    $this->checkUserAccess($editUrl, TestSupport::ADMINISTRATOR, 200);
+    $this->assertText('Current workflow state: Under evaluation');
+    $this->checkUserAccess($stateChangeUrl, TestSupport::ADMINISTRATOR, 200);
+    $this->checkUserAccess($editUrl, TestSupport::IUCN_MANAGER, 200);
+    $this->checkUserAccess($stateChangeUrl, TestSupport::IUCN_MANAGER, 200);
+    $this->checkUserAccess($editUrl, TestSupport::COORDINATOR1, 200);
+    $this->checkUserAccess($stateChangeUrl, TestSupport::COORDINATOR1, 200);
+    $this->checkUserAccess($editUrl, TestSupport::COORDINATOR2, 403);
+    $this->checkUserAccess($stateChangeUrl, TestSupport::COORDINATOR2, 403);
+    $this->checkUserAccess($editUrl, TestSupport::ASSESSOR1, 403);
+    $this->checkUserAccess($stateChangeUrl, TestSupport::ASSESSOR1, 403);
+    $this->checkUserAccess($editUrl, TestSupport::REVIEWER1, 403);
+    $this->checkUserAccess($stateChangeUrl, TestSupport::REVIEWER1, 403);
+
+    $assessor = user_load_by_mail(TestSupport::ASSESSOR1);
+    $this->userLogIn(TestSupport::COORDINATOR1);
+    $this->drupalPostForm($stateChangeUrl, ['field_assessor' => $assessor->id()], static::TRANSITION_LABELS[AssessmentWorkflow::STATUS_UNDER_ASSESSMENT]);
+  }
+}
