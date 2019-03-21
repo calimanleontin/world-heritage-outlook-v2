bb591a32ebcde04fbed864fa483a38757420be3d
stefan.butura <stefan_butura@yahoo.com>
2019-03-21 13:41:59 +0200

    refs #6254 - update core to 8.6.13

--- docroot/core/misc/states.es6.js
+++ docroot/core/misc/states.es6.js
@@ -698,7 +698,7 @@
       if (e.value) {
         const label = `label${e.target.id ? `[for=${e.target.id}]` : ''}`;
         const $label = $(e.target)
-          .attr({ required: 'required', 'aria-required': 'aria-required' })
+          .attr({ required: 'required', 'aria-required': 'true' })
           .closest('.js-form-item, .js-form-wrapper')
           .find(label);
         // Avoids duplicate required markers on initialization.
