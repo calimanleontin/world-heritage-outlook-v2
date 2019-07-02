a60ceee213144577819a3680d7ffb3519c830221
Stefan <stefanbutura9@gmail.com>
2019-07-02 15:33:09 +0300

    refs #6378 - work on export.

--- docroot/modules/iucn/iucn_assessment/src/Controller/IucnExportController.php
+++ docroot/modules/iucn/iucn_assessment/src/Controller/IucnExportController.php
@@ -3,7 +3,11 @@
 namespace Drupal\iucn_assessment\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\FieldableEntityInterface;
 use Drupal\node\NodeInterface;
+use Drupal\paragraphs\ParagraphInterface;
+use HTMLtoOpenXML\Parser;
+use PhpOffice\PhpWord\TemplateProcessor;
 
 class IucnExportController extends ControllerBase {
 
@@ -13,16 +17,84 @@ class IucnExportController extends ControllerBase {
    * @param \Drupal\node\NodeInterface $node
    */
   public function docExport(NodeInterface $node) {
-    //    //Open template with ${table}
-    //    $template_document = new \PhpOffice\PhpWord\TemplateProcessor('/home/stefan/Desktop/template.docx');
-    //
-    //    // Replace mark by xml code of table
-    //    $template_document->setValue('title', 'xddd');
-    //
-    //    //save template with table
-    //    $template_document->saveAs('/home/stefan/Desktop/template-res.docx');
-    //
-    return ['#markup' => 'xd'];
+    $templateDocument = new \PhpOffice\PhpWord\TemplateProcessor(drupal_get_path('module', 'iucn_assessment') . '/data/export/assessment_export_tpl.docx');
+    $fields = $this->getTemplateFields($templateDocument);
+
+    foreach ($fields as $field => $referencedFields) {
+      if (!is_array($referencedFields)) {
+        $templateDocument->setValue($field, $this->getFieldValue($node, $field));
+        continue;
+      }
+
+      $paragraphFieldList = $node->get($field);
+      $arrayKeys = array_keys($referencedFields);
+      $firstParagraphField = reset($arrayKeys);
+      $templateDocument->cloneRow($firstParagraphField, count($paragraphFieldList));
+
+      if ($firstParagraphField == 'field_as_values_wh.index') {
+        $templateDocument->cloneRow($firstParagraphField, count($paragraphFieldList));
+      }
+
+      foreach ($paragraphFieldList as $idx => $fieldItem) {
+        $this->writeParagraphToTemplate($templateDocument, $fieldItem->entity, $referencedFields, $idx + 1);
+      }
+
+    }
+
+    $templateDocument->saveAs('/home/stefan/work/iucn/docroot/sites/default/files/template-res.docx');
+    return ['#markup' => 'xd', '#cache' => ['max-age' => 0]];
+  }
+
+  protected function getFieldValue(FieldableEntityInterface $entity, $field) {
+    $fieldRender = $entity->get($field)->view(['label' => 'hidden']);
+    $fieldRender = \Drupal::service('renderer')->render($fieldRender);
+    $fieldRender = $this->stripTagsContent($fieldRender, '<button>', TRUE);
+    return strip_tags($fieldRender);
+  }
+
+  protected function writeParagraphToTemplate(TemplateProcessor $templateProcessor, ParagraphInterface $paragraph, $fields, $index) {
+    foreach ($fields as $templateVariable => $field) {
+      if (!$paragraph->hasField($field)) {
+        continue;
+      }
+
+      $templateProcessor->setValue("$templateVariable#$index", $this->getFieldValue($paragraph, $field), 2);
+//      $templateProcessor->replaceBlock()
+    }
+  }
+
+  protected function getTemplateFields(TemplateProcessor $templateDocument) {
+    $variables = $templateDocument->getVariables();
+    $fields = [];
+    foreach ($variables as $variable) {
+      if (strpos($variable, '.') !== FALSE) {
+        $explode = explode('.', $variable);
+        $fields[$explode[0]][$variable] = $explode[1];
+      }
+      else {
+        $fields[$variable] = $variable;
+      }
+    }
+
+    return $fields;
+  }
+
+  protected function stripTagsContent($text, $tags = '', $invert = FALSE) {
+    preg_match_all('/<(.+?)[\s]*\/?[\s]*>/si', trim($tags), $tags);
+    $tags = array_unique($tags[1]);
+
+    if(is_array($tags) AND count($tags) > 0) {
+      if($invert == FALSE) {
+        return preg_replace('@<(?!(?:'. implode('|', $tags) .')\b)(\w+)\b.*?>.*?</\1>@si', '', $text);
+      }
+      else {
+        return preg_replace('@<('. implode('|', $tags) .')\b.*?>.*?</\1>@si', '', $text);
+      }
+    }
+    elseif($invert == FALSE) {
+      return preg_replace('@<(\w+)\b.*?>.*?</\1>@si', '', $text);
+    }
+    return $text;
   }
 
 }
