58887eca94c793c7b28a710f353a2d7ec7ad9040
EdW <ramona.stefan@eaudeweb.ro>
2019-08-05 11:03:23 +0300

    .

--- docroot/modules/iucn/iucn_assessment/tests/src/Functional/Workflow/Workflow03UnderAssessmentPhaseTest.php
+++ docroot/modules/iucn/iucn_assessment/tests/src/Functional/Workflow/Workflow03UnderAssessmentPhaseTest.php
@@ -14,29 +14,29 @@ use Drupal\Tests\iucn_assessment\Functional\TestSupport;
 class Workflow03UnderAssessmentPhaseTest extends WorkflowTestBase {
 
   const WORKFLOW_STATE = AssessmentWorkflow::STATUS_UNDER_ASSESSMENT;
-//
-//  public function testUnderAssessmentPhaseAccess() {
-//    $this->checkUserAccess($this->editUrl, TestSupport::ADMINISTRATOR, 200);
-//    $this->assertSession()->pageTextContains('Current workflow state: Being assessed');
-//    $this->checkUserAccess($this->stateChangeUrl, TestSupport::ADMINISTRATOR, 200);
-//    $this->checkUserAccess($this->editUrl, TestSupport::IUCN_MANAGER, 200);
-//    $this->checkUserAccess($this->stateChangeUrl, TestSupport::IUCN_MANAGER, 200);
-//    $this->checkUserAccess($this->editUrl, TestSupport::COORDINATOR1, 403);
-//    $this->checkUserAccess($this->stateChangeUrl, TestSupport::COORDINATOR1, 403);
-//    $this->checkUserAccess($this->editUrl, TestSupport::COORDINATOR2, 403);
-//    $this->checkUserAccess($this->stateChangeUrl, TestSupport::COORDINATOR2, 403);
-//    $this->checkUserAccess($this->editUrl, TestSupport::ASSESSOR1, 200);
-//    $this->checkUserAccess($this->stateChangeUrl, TestSupport::ASSESSOR1, 200);
-//    $this->checkUserAccess($this->editUrl, TestSupport::REVIEWER1, 403);
-//    $this->checkUserAccess($this->stateChangeUrl, TestSupport::REVIEWER1, 403);
-//    $this->checkUserAccess($this->editUrl, TestSupport::REFERENCES_REVIEWER1, 403);
-//    $this->checkUserAccess($this->stateChangeUrl, TestSupport::REFERENCES_REVIEWER1, 403);
-//    $this->checkUserAccess($this->editUrl, TestSupport::REFERENCES_REVIEWER2, 403);
-//    $this->checkUserAccess($this->stateChangeUrl, TestSupport::REFERENCES_REVIEWER2, 403);
-//
-//    $this->userLogIn(TestSupport::ASSESSOR1);
-//    $this->drupalPostForm($this->stateChangeUrl, [], static::TRANSITION_LABELS[AssessmentWorkflow::STATUS_READY_FOR_REVIEW]);
-//  }
+
+  public function testUnderAssessmentPhaseAccess() {
+    $this->checkUserAccess($this->editUrl, TestSupport::ADMINISTRATOR, 200);
+    $this->assertSession()->pageTextContains('Current workflow state: Being assessed');
+    $this->checkUserAccess($this->stateChangeUrl, TestSupport::ADMINISTRATOR, 200);
+    $this->checkUserAccess($this->editUrl, TestSupport::IUCN_MANAGER, 200);
+    $this->checkUserAccess($this->stateChangeUrl, TestSupport::IUCN_MANAGER, 200);
+    $this->checkUserAccess($this->editUrl, TestSupport::COORDINATOR1, 403);
+    $this->checkUserAccess($this->stateChangeUrl, TestSupport::COORDINATOR1, 403);
+    $this->checkUserAccess($this->editUrl, TestSupport::COORDINATOR2, 403);
+    $this->checkUserAccess($this->stateChangeUrl, TestSupport::COORDINATOR2, 403);
+    $this->checkUserAccess($this->editUrl, TestSupport::ASSESSOR1, 200);
+    $this->checkUserAccess($this->stateChangeUrl, TestSupport::ASSESSOR1, 200);
+    $this->checkUserAccess($this->editUrl, TestSupport::REVIEWER1, 403);
+    $this->checkUserAccess($this->stateChangeUrl, TestSupport::REVIEWER1, 403);
+    $this->checkUserAccess($this->editUrl, TestSupport::REFERENCES_REVIEWER1, 403);
+    $this->checkUserAccess($this->stateChangeUrl, TestSupport::REFERENCES_REVIEWER1, 403);
+    $this->checkUserAccess($this->editUrl, TestSupport::REFERENCES_REVIEWER2, 403);
+    $this->checkUserAccess($this->stateChangeUrl, TestSupport::REFERENCES_REVIEWER2, 403);
+
+    $this->userLogIn(TestSupport::ASSESSOR1);
+    $this->drupalPostForm($this->stateChangeUrl, [], static::TRANSITION_LABELS[AssessmentWorkflow::STATUS_READY_FOR_REVIEW]);
+  }
 
   public function testReadOnlyAccessForAssessor() {
     \Drupal::service('theme_handler')->install(['iucn_backend']);
