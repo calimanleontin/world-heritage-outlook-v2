bd9ee4ba1f64f90aa204b7842110678a3a560f65
Stefan <stefanbutura9@gmail.com>
2019-08-06 11:52:02 +0300

    Remove sleep.

--- docroot/modules/iucn/iucn_assessment/tests/src/Functional/Workflow/Workflow02JsUnderEvaluationPhaseTest.php
+++ docroot/modules/iucn/iucn_assessment/tests/src/Functional/Workflow/Workflow02JsUnderEvaluationPhaseTest.php
@@ -36,7 +36,6 @@ class Workflow02JsUnderEvaluationPhaseTest extends IucnAssessmentWebDriverTestBa
     $this->click('#edit-field-as-values-wh-0-top-actions-buttons-delete');
     $assert_session = $this->assertSession();
     $assert_session->waitForElement('css', '#drupal-modal');
-    sleep(10);
     $this->assertSession()->responseContains('This value cannot be deleted because it is the only affected value for the some threats. Please edit or delete these threats first');
   }
 
