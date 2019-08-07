083732d2dae0e1a5d3d4c01a57431d1c6672718d
Stefan <stefanbutura9@gmail.com>
2019-08-07 13:31:27 +0300

    Create changestateformtest.

---
+++ docroot/modules/iucn/iucn_assessment/tests/src/Functional/Form/ChangeStateFormTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\Tests\iucn_assessment\Functional\Form;
+
+use Drupal\Tests\iucn_assessment\Functional\Workflow\WorkflowTestBase;
+
+/**
+ * @group iucn
+ */
+class ChangeStateFormTest extends WorkflowTestBase  {
+
+  public function testValidation() {
+
+  }
+
+}
