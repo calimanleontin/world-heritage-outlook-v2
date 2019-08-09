506bdf7ec474289366b202fb06baba5f34e81411
EdW <ramona.stefan@eaudeweb.ro>
2019-08-09 15:34:34 +0300

    fix testReviewingReferencesPhaseAccess

--- docroot/modules/iucn/iucn_assessment/tests/src/Functional/Workflow/Workflow08ReviewingReferencesPhaseTest.php
+++ docroot/modules/iucn/iucn_assessment/tests/src/Functional/Workflow/Workflow08ReviewingReferencesPhaseTest.php
@@ -4,6 +4,7 @@ namespace Drupal\Tests\iucn_assessment\Functional\Workflow;
 
 use Drupal\Core\Url;
 use Drupal\iucn_assessment\Plugin\AssessmentWorkflow;
+use Drupal\node\Entity\Node;
 use Drupal\Tests\iucn_assessment\Functional\TestSupport;
 
 /**
@@ -16,6 +17,48 @@ class Workflow08ReviewingReferencesPhaseTest extends WorkflowTestBase {
   const WORKFLOW_STATE = AssessmentWorkflow::STATUS_REVIEWING_REFERENCES;
 
   public function testReviewingReferencesPhaseAccess() {
+    /** @var \Drupal\user\Entity\User $reviewer1 */
+    $reviewer1 = user_load_by_mail(TestSupport::REVIEWER1);
+    /** @var \Drupal\user\Entity\User $reviewer2 */
+    $reviewer2 = user_load_by_mail(TestSupport::REVIEWER2);
+    /** @var \Drupal\user\Entity\Use $referencesReviewer1 */
+    $referencesReviewer1 = user_load_by_mail(TestSupport::REFERENCES_REVIEWER1);
+    $assessment = $this->createMockAssessmentNode(AssessmentWorkflow::STATUS_READY_FOR_REVIEW, []);
+    $this->editUrl = $assessment->toUrl('edit-form');
+    $this->stateChangeUrl = Url::fromRoute('iucn_assessment.node.state_change', ['node' => $assessment->id()]);
+
+    $this->userLogIn(TestSupport::COORDINATOR1);
+    $this->drupalGet($this->stateChangeUrl);
+    $label = t(WorkflowTestBase::TRANSITION_LABELS[AssessmentWorkflow::STATUS_UNDER_REVIEW]);
+    $this->click("[value=\"{$label}\"]");
+
+    $reviewer1Revision = $this->workflowService->getReviewerRevision($assessment,$reviewer1->id());
+    $reviewer2Revision = $this->workflowService->getReviewerRevision($assessment,$reviewer2->id());
+    $this->userLogIn(TestSupport::REVIEWER1);
+    $this->stateChangeUrl = Url::fromRoute('iucn_assessment.node_revision.state_change', [
+      'node' => $reviewer1Revision->id(),
+      'node_revision' => $reviewer1Revision->getRevisionId(),
+    ]);
+    $this->drupalGet($this->stateChangeUrl);
+    $label = t(WorkflowTestBase::TRANSITION_LABELS[AssessmentWorkflow::STATUS_FINISHED_REVIEWING]);
+    $this->click("[value=\"{$label}\"]");
+
+    $this->userLogIn(TestSupport::REVIEWER2);
+    $this->stateChangeUrl = Url::fromRoute('iucn_assessment.node_revision.state_change', [
+      'node' => $reviewer2Revision->id(),
+      'node_revision' => $reviewer2Revision->getRevisionId(),
+    ]);
+    $this->drupalGet($this->stateChangeUrl);
+    $label = t(WorkflowTestBase::TRANSITION_LABELS[AssessmentWorkflow::STATUS_FINISHED_REVIEWING]);
+    $this->click("[value=\"{$label}\"]");
+    drupal_flush_all_caches();
+    $this->userLogIn(TestSupport::COORDINATOR1);
+    $this->stateChangeUrl = Url::fromRoute('iucn_assessment.node.state_change', ['node' => $assessment->id()]);
+    $this->drupalPostForm($this->stateChangeUrl, [], static::TRANSITION_LABELS[AssessmentWorkflow::STATUS_UNDER_COMPARISON]);
+    $this->drupalPostForm($this->stateChangeUrl, ['field_references_reviewer' => $referencesReviewer1->id()], static::TRANSITION_LABELS[AssessmentWorkflow::STATUS_REVIEWING_REFERENCES]);
+
+    $this->editUrl = $assessment->toUrl('edit-form');
+    $this->stateChangeUrl = Url::fromRoute('iucn_assessment.node.state_change', ['node' => $assessment->id()]);
     $this->checkUserAccess($this->editUrl, TestSupport::ADMINISTRATOR, 200);
     $this->assertSession()->pageTextContains('Current workflow state: Reference standardisation');
     $this->checkUserAccess($this->stateChangeUrl, TestSupport::ADMINISTRATOR, 200);
