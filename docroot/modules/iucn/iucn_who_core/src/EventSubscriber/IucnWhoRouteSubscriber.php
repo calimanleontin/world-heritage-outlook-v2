eb5fc55f2692c7ae6794c1b4e1b98cd5b7983eaa
leontin.caliman <leontin.caliman@eaudeweb.ro>
2019-09-09 13:56:57 +0300

    refs #7280 do not display error message if user not found

---
+++ docroot/modules/iucn/iucn_who_core/src/EventSubscriber/IucnWhoRouteSubscriber.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\iucn_who_core\EventSubscriber;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Drupal\Core\Routing\RoutingEvents;
+use Drupal\iucn_who_core\Form\IucnUserPasswordForm;
+use Symfony\Component\Routing\RouteCollection;
+
+class IucnWhoRouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      RoutingEvents::ALTER => ['onAlterRoutes', -9999],
+    ];
+  }
+
+  public function alterRoutes(RouteCollection $collection) {
+    $route = $collection->get('user.pass');
+    if ($route) {
+      $route->setDefault('_form', IucnUserPasswordForm::class);
+    }
+  }
+}
