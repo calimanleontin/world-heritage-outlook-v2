fdfbf52a89c22c70727f9c5ddf55a4ed031e9e77
Stefan <stefanbutura9@gmail.com>
2019-09-25 19:18:03 +0300

    refs #7280 - refactor.

--- docroot/modules/iucn/iucn_who_core/src/Form/IucnUserPasswordForm.php
+++ docroot/modules/iucn/iucn_who_core/src/Form/IucnUserPasswordForm.php
@@ -14,19 +14,36 @@ class IucnUserPasswordForm extends UserPasswordForm {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    parent::validateForm($form, $form_state);
-    if (!empty($form_state->getErrors())) {
-      $form_state->clearErrors();
-    }
+    // Do nothing here since we don't want to validate the username.
   }
 
+  /**
+   * {@inheritDoc}
+   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    if ($account = $form_state->getValue('account')) {
-      parent::submitForm($form, $form_state);
-      return;
+    $name = trim($form_state->getValue('name'));
+    // Try to load by email.
+    $users = $this->userStorage->loadByProperties(['mail' => $name]);
+    if (empty($users)) {
+      // No success, try to load by name.
+      $users = $this->userStorage->loadByProperties(['name' => $name]);
+    }
+    $account = reset($users);
+    if ($account && $account->id()) {
+      // Blocked accounts cannot request a new password.
+      if ($account->isActive()) {
+        $langcode = $this->languageManager->getCurrentLanguage()->getId();
+
+        $mail = _user_mail_notify('password_reset', $account, $langcode);
+        if (!empty($mail)) {
+          $this->logger('user')->notice('Password reset instructions mailed to %name at %email.', ['%name' => $account->getAccountName(), '%email' => $account->getEmail()]);
+        }
+      }
     }
 
-    drupal_set_message($this->t('If you have registered an account, we\'ve sent you an email with further information to recover your password. Check your Inbox (and Spam folder)'));
+    // Always set this message, even if the user doesn't exist.
+    $this->messenger()->addStatus($this->t('If you have registered an account, we\'ve sent you an email with further information to recover your password. Check your Inbox (and Spam folder)'));
     $form_state->setRedirect('user.page');
   }
+
 }
